747da3ac78cfcff49f17e44710689d12c8795fc0
Ronja Kræmer <rkhansen01@gmail.com>
2021-11-21 18:37:11 +0100

    add waypoint_manager_node framework
    Signed-off-by: Ronja Kræmer <rkhansen01@gmail.com>

--- navigation/waypoint_manager/scripts/waypoint_manager_node.py
+++ navigation/waypoint_manager/scripts/waypoint_manager_node.py
@@ -1,1 +1,47 @@
 #!/usr/bin/python3
+# written by Ronja Kræmer, Student
+
+import rospy
+from vortex_msgs.srv import WaypointUpdate
+
+
+class WaypointManager():
+    def __init__(self):
+        rospy.init_node('WaypointManager')
+        update_service = rospy.Service('/update_waypoint', WaypointUpdate, self.send_next_waypoint)
+        
+        self.waypoint_list = []
+
+    def send_next_waypoint(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	try:
+		waypoint_manager = WaypointManager()
+
+		rospy.spin()
+	except rospy.ROSInterruptException:
+		pass
