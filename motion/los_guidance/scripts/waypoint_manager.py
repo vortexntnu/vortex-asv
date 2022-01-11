eab1b426be6f68c3a794fff131d50ab808a1472d
Vortex HP <your_email@example.com>
2022-01-11 14:02:51 +0100

    make file structure
    Signed-off-by: Vortex HP <your_email@example.com>

---
+++ motion/los_guidance/scripts/waypoint_manager.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+
+import rospy
+
+
+
+
+
+
+class WaypointManager:
+    """
+	description - fill in
+	"""
+    
+    
+    def __init__(self):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	try:
+		waypoint_manager = WaypointManager()     # class
+
+		rospy.spin()
+	except rospy.ROSInterruptException:
+		pass
