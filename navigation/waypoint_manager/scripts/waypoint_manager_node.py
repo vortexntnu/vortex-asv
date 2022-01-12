a42a4e1f23791a4992f589353541575599c6be45
Vortex HP <your_email@example.com>
2022-01-12 17:31:48 +0100

    create add/remove waypoint services
    Signed-off-by: Vortex HP <your_email@example.com>

--- navigation/waypoint_manager/scripts/waypoint_manager_node.py
+++ navigation/waypoint_manager/scripts/waypoint_manager_node.py
@@ -3,16 +3,17 @@
 
 import rospy
 import actionlib
-from vortex_msgs.msg import LosPathFollowingAction, LosPathFollowingGoal, LosPathFollowingResult, LosPathFollowingFeedback
+from vortex_msgs.msg import (
+    LosPathFollowingAction,
+    LosPathFollowingGoal,
+    LosPathFollowingResult,
+    LosPathFollowingFeedback,
+)
+from vortex_msgs.srv import Waypoint, WaypointRequest, WaypointResponse
 
 
-class WaypointManager():
-    """"
-    
-
-    Attributes:
-        _goal       A vortex_msgs action that contains the next waypoint
-
+class WaypointManager:
+    """ 
     Nodes created:
         WaypointManager
 
@@ -20,37 +21,41 @@ class WaypointManager():
 
     Publishes to:
 
-
     """
-                
 
     def __init__(self):
-        rospy.init_node('WaypointManager')
+        rospy.init_node("WaypointManager")
 
         self.waypoint_list = []
 
         # Action client
-        self.action_client = actionlib.SimpleActionClient(name='los_action_server', ActionSpec=LosPathFollowingAction)
+        self.action_client = actionlib.SimpleActionClient(
+            name="los_action_server", ActionSpec=LosPathFollowingAction
+        )
         self.action_client.wait_for_server()
 
-    
-    def addWaypointToList():
-       pass
+        # Services
+        self.add_waypoint_service = rospy.Service(
+            "add_waypoint", Waypoint, self.add_waypoint_to_list
+        )
+        self.remove_waypoint_service = rospy.Service(
+            "remove_waypoint", Waypoint, self.remove_waypoint_from_list
+        )
 
-    def removeWaypointFromList():
+    def add_waypoint_to_list(self, req):
         pass
 
-
-
+    def remove_waypoint_from_list(self, req):
+        pass
 
     def spin(self):
-        index_waypoint_k = 0 
+        index_waypoint_k = 0
         while not rospy.is_shutdown():
             if len(self.waypoint_list) >= 2:
-                if index_waypoint_k < len(self.waypoint_list)-1:
-                    goal = LosPathFollowingGoal()  
-                    self.goal.waypoints[0]=self.waypoint_list[index_waypoint_k]
-                    self.goal.waypoints[1]=self.waypoint_list[index_waypoint_k+1]
+                if index_waypoint_k < len(self.waypoint_list) - 1:
+                    goal = LosPathFollowingGoal()
+                    goal.waypoints[0] = self.waypoint_list[index_waypoint_k]
+                    goal.waypoints[1] = self.waypoint_list[index_waypoint_k + 1]
                     self.action_client.send_goal(self._goal)
                     self.action_client.wait_for_result()
                     index_waypoint_k += 1
@@ -58,12 +63,10 @@ class WaypointManager():
                     self.waypoint_list.clear()
 
 
+if __name__ == "__main__":
+    try:
+        waypoint_manager = WaypointManager()
 
-
-if __name__ == '__main__':
-	try:
-		waypoint_manager = WaypointManager()
-
-		waypoint_manager.spin()
-	except rospy.ROSInterruptException:
-		pass
+        waypoint_manager.spin()
+    except rospy.ROSInterruptException:
+        pass
