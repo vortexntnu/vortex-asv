5a4ae50ad61c712effdb28f7c6b31c4998ab2d0f
Vortex HP <your_email@example.com>
2022-01-12 15:47:47 +0100

    write spin-loop in WaypointManager
    Signed-off-by: Vortex HP <your_email@example.com>

--- navigation/waypoint_manager/scripts/waypoint_manager_node.py
+++ navigation/waypoint_manager/scripts/waypoint_manager_node.py
@@ -7,37 +7,55 @@ from vortex_msgs.msg import LosPathFollowingAction, LosPathFollowingGoal, LosPat
 
 
 class WaypointManager():
+    """"
+    
+
+    Attributes:
+        _goal       A vortex_msgs action that contains the next waypoint
+
+    Nodes created:
+        WaypointManager
+
+    Subscribes to:
+
+    Publishes to:
+
+
+    """
+                
+
     def __init__(self):
         rospy.init_node('WaypointManager')
-        self.client = actionlib.SimpleActionClient(name='los_action_server', ActionSpec=LosPathFollowingAction)
 
-        
         self.waypoint_list = []
 
+        # Action client
+        self.action_client = actionlib.SimpleActionClient(name='los_action_server', ActionSpec=LosPathFollowingAction)
+        self.action_client.wait_for_server()
+
+    
+    def addWaypointToList():
+       pass
+
+    def removeWaypointFromList():
+        pass
+
+
+
+
     def spin(self):
+        index_waypoint_k = 0 
         while not rospy.is_shutdown():
-            pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            if len(self.waypoint_list) >= 2:
+                if index_waypoint_k < len(self.waypoint_list)-1:
+                    goal = LosPathFollowingGoal()  
+                    self.goal.waypoints[0]=self.waypoint_list[index_waypoint_k]
+                    self.goal.waypoints[1]=self.waypoint_list[index_waypoint_k+1]
+                    self.action_client.send_goal(self._goal)
+                    self.action_client.wait_for_result()
+                    index_waypoint_k += 1
+                else:
+                    self.waypoint_list.clear()
 
 
 
