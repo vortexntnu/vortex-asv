c1620eac004a7dbb0575ccf0f16bf11e84c9eb19
Viktor Naas <viktorn@stud.ntnu.no>
2022-02-03 16:45:26 +0100

    added node initialization and preempted a minor possible bug

--- mission/finite_state_machine/src/dp_client.py
+++ mission/finite_state_machine/src/dp_client.py
@@ -7,8 +7,7 @@ from geometry_msgs import Pose, PoseStamped
 from rospy.core import rospydebug
 from tf.transformations import (
     euler_from_quaternion,
-    quaternion_from_euler,
-    create
+    quaternion_from_euler
 )
 from move_base_msgs.msg import MoveBaseAction, MoveBaseActionResult, MoveBaseActionGoal
 from vortex_msgs.srv import ControlMode, ControlModeRequest, ControlModeResponse
@@ -31,20 +30,28 @@ def change_control_mode_client(requested_mode):
         print("Service call failed: %s"%e)
 
 def dp_move_base(x, y, yaw, controlMode):
-    #action client
-    action_client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-    action_client.wait_for_server()
-    goal = MoveBaseActionGoal()
-
-    goal.target_pose.pose.position.x = x
-    goal.target_pose.pose.position.y = y
-    goal.target_pose.pose.orientation = quaternion_from_euler(0, 0, yaw)
-    action_client.send_goal(goal)
     
-    action_client.wait_for_result()
-
-    #service
     control_mode_service = rospy.Service(
         "set_control_mode", ControlMode, change_control_mode_client
     )
 
+    #action client
+    action_client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+    action_client.wait_for_server()
+    goal = MoveBaseActionGoal()
+    #create goal
+    goal.target_pose.pose.position.x = x
+    goal.target_pose.pose.position.y = y
+    goal.target_pose.pose.orientation = quaternion_from_euler(0, 0, yaw)
+    action_client.send_goal(goal)
+    #wait for server to finish action
+    action_client.wait_for_result()
+
+    #service
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('dp_move_base')
+        dp_move_base(0,0,0,OPEN_LOOP)
+    except rospy.ROSInternalException as e:
+        rospy.logerr(e)
