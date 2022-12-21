407778a35604e21460638514cdeab9c402e5bd19
hannah laugaland <hannahchaplin@hotmail.no>
2022-12-21 17:37:33 +0100

    write test for KF

---
+++ navigation/tracking/scripts/test/test_tracker.py
@@ -0,0 +1,146 @@
+import numpy as np
+
+def dummy(number):
+    return number**2
+
+def test_dummy():
+    num = 2
+
+    print("running dummy test")
+
+    assert (num**2 == dummy(num))
+
+#-------------------------------------
+
+class Tracker:
+
+    def __init__(self):
+        #x = [r, thetha, r', theta']
+
+        self.time_step = 0.002
+        self.x_pri = np.ndarray((4,), buffer = np.array(
+            [1.0, 1.0, 1.0, 1.0]), 
+            dtype=float)
+        self.P_pri = np.ndarray((4,4), buffer=np.array([
+            [0.1,0,  0,  0], 
+            [0,  0.1,0,  0], 
+            [0,  0,  0.1,0], 
+            [0,  0,  0,  0.1]]), 
+            dtype = float)
+
+        self.x_post = np.ndarray((4,), dtype=float)
+        self.P_post = np.ndarray((4,4), dtype=float)
+
+        self.L = np.ndarray((2,2), dtype=float)
+
+        self.A = np.ndarray((4,4), buffer=np.array([
+            [1, 0, 1, 0], 
+            [0, 1, 0, 1],
+            [0, 0, 1, 0], #assuming constnat velocity
+            [0, 0, 0, 1]]),#assuming constnat velocity
+            dtype = float)
+
+        self.C = np.ndarray((2,4), buffer=np.array([
+            [1, 0, 0, 0], 
+            [0, 1, 0, 0]]),
+            dtype = float)
+
+        self.Q = np.ndarray((4,4), buffer=np.array([
+            [0.001, 0, 0, 0], 
+            [0, 0.001, 0, 0],
+            [0, 0, 0.1, 0], 
+            [0, 0, 0, 0.1]]),
+            dtype = float)
+
+        self.R = np.ndarray((2,2), buffer=np.array([
+            [0.1, 0], 
+            [0, 0.1]]),
+            dtype = float)
+
+    def prediction_step(self):
+        self.x_pri = np.matmul(self.A,self.x_post)
+        self.P_pri = np.matmul(self.A, np.matmul(self.P_post,self.A.T))+ self.Q
+
+    def correction_step(self, y):
+
+        temp1 = np.matmul(self.P_pri, np.transpose(self.C))
+        temp2 = np.matmul(self.C, temp1)
+        self.L = np.matmul(temp1,np.linalg.inv(temp2 + self.R))
+
+        temp3 = np.matmul(self.C, self.x_pri)
+        self.x_post = self.x_pri + np.matmul(self.L,(y - temp3))
+
+        temp4 = (np.identity(len(self.x_pri))-np.matmul(self.L,self.C))
+        temp5 = np.matmul(self.L, np.matmul(self.R, np.transpose(self.L)))
+        self.P_post = np.matmul(temp4, np.matmul(self.P_pri,np.transpose(temp4))) + temp5
+
+
+#-----------------------------------
+
+def test_init():
+    try: 
+        tracker = Tracker()
+    except:
+        assert False
+
+def test_prediction():
+
+    try: 
+        tracker = Tracker()
+        tracker.prediction_step()
+    except:
+        assert False
+
+def test_correction():
+    y = np.ndarray((2,), buffer = np.array([1,2]), dtype = float)
+
+    try: 
+        tracker = Tracker()
+        tracker.correction_step(y)
+    except:
+        assert False
+
+def test_KF_highQ():
+    """
+    Here we set the variance of disturbance high. 
+    Thus we expect the final estiamtes to be close to the measurments. 
+    We simulate a baot standing still. 
+    """
+
+    r = 0.5
+    theta = 0.3
+    tollerance = 0.1
+
+    tracker = Tracker()
+
+    for i in range (len(tracker.x_post)):
+        tracker.Q[i,i] = 10
+
+    measurments = np.ndarray((20,2), dtype = float)
+    for i in range(20):
+        measurments[i, 0] = r
+        measurments[i, 1] = theta
+
+    for y in measurments:
+        tracker.correction_step(y)
+        tracker.prediction_step()
+
+    print("posterior estimates: ", tracker.x_post)
+
+    assert abs(tracker.x_post[0] - r)     < tollerance  
+    assert abs(tracker.x_post[1] - theta) < tollerance  
+    assert abs(tracker.x_post[2])         < tollerance  
+    assert abs(tracker.x_post[3])         < tollerance  
+
+
+
+
+
+
+
+
+
+
+
+
+
--- navigation/tracking/scripts/traking_node.py
+++ navigation/tracking/scripts/traking_node.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 
 import rospy
+import numpy as np
 
 
 from geometry_msgs.msg import PoseStamped, Point
+
+"""
+Task: Estimate position and velocity of surronding boats. 
+Sub task: Detect boats based on LIDAR data. 
+Sub task: Distinguish boats from each other and other objects.
+Sub task: Estimate position and velocity for each boat realtive to the vessel (given measurements for the boats position).
+    - Set up KF. 
+        Use Kalmanfilter with 4 states: r - radius, theta - angle, r_der - change in radius, thetha_der - change in angle.
+        There are 2 measurments: r and theta. 
+        Define A and C matricies. B and u are 0. 
+        Define Q and R matricies. Can R be updated real time based on the certanty of the detections? 
+    - Should we use a different type of KF? 
+"""
 
 
 class Tracker:
@@ -16,6 +30,44 @@ class Tracker:
     def __init__(self):
         rospy.init_node("Tracker")
 
+        #x = [r, thetha, r', theta']
+
+        self.time_step = 0.002
+        self.x_pri = np.ones(4)
+        self.P_pri = np.matrix([
+            [0.1,0,  0,  0], 
+            [0,  0.1,0,  0], 
+            [0,  0,  0.1,0], 
+            [0,  0,  0,  0.1]])
+
+        self.x_post = np.ones(4)
+        self.P_post = np.matrix([
+            [0.1,0,  0,  0], 
+            [0,  0.1,0,  0], 
+            [0,  0,  0.1,0], 
+            [0,  0,  0,  0.1]])
+
+        self.A = np.matrix([
+            [1, 0, 1, 0], 
+            [0, 1, 0, 1],
+            [0, 0, 1, 0], #assuming constnat velocity
+            [0, 0, 0, 1]])#assuming constnat velocity
+
+        self.C = np.matrix([
+            [1, 0, 0, 0], 
+            [0, 1, 0, 0]]
+        )
+
+        self.Q = np.matrix([
+            [0.001, 0, 0, 0], 
+            [0, 0.001, 0, 0],
+            [0, 0, 0.1, 0], 
+            [0, 0, 0, 0.1]])
+
+        self.R = np.matrix([
+            [0.1, 0], 
+            [0, 0.1]])
+
     def spin(self):
 
         while not rospy.is_shutdown():
@@ -25,8 +77,8 @@ class Tracker:
 
 if __name__ == "__main__":
     try:
-        waypoint_manager = Tracker()
+        tracker = Tracker()
 
-        waypoint_manager.spin()
+        tracker.spin()
     except rospy.ROSInterruptException:
         pass
