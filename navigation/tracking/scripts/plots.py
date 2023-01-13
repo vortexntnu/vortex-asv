747e41e4b3119ee8bab2ba5fb9d981e8984c0c9e
hannah laugaland <hannahchaplin@hotmail.no>
2023-01-13 14:34:18 +0100

    create interactive plot

--- navigation/tracking/scripts/plots.py
+++ navigation/tracking/scripts/plots.py
@@ -3,6 +3,8 @@ sys.path.insert(0,'/home/hannahcl/Documents/vortex/monkey_tracking/data_generati
 from scenarios import BaseScenario
 import matplotlib.pyplot as plt
 from utility import time_from_step
+from track_manager import TRACK_STATUS
+
 
 
 def plot_with_estimates(scenario, measurements, ground_truths, estimates):
@@ -124,6 +126,106 @@ def plot_tentative_confirm_del(scenario, measurements, ground_truths, tentative_
     plt.gca().set_aspect("equal")
     plt.grid()
     plt.show()
+
+
+def plot_interactive(
+    scenario, 
+    measurements, 
+    ground_truths, 
+    tentative_estimates, 
+    conf_estimates, 
+    tentative_del_estimates, 
+    estimate_status,
+    wait_for_btn_press
+    ):
+
+    plt.ion()
+
+    end_time = time_from_step(scenario.k, scenario.config.dt)
+    min_alpha = 0.5
+
+    # Ground truths
+    for target in ground_truths:
+        track = target.track
+        x = track[:, 1]
+        y = track[:, 2]
+        #alpha = 1 - np.vectorize(max)(min_alpha, 1 - track[:, 5] / end_time)
+        alpha = None
+        plt.scatter(x, y, alpha=alpha)
+
+    plt.xlabel("x [m]")
+    plt.ylabel("y [m]")
+
+    plt.gca().set_aspect("equal")
+    plt.grid()
+    plt.show()
+
+    # Measurements
+    k_ten = 0
+    k_conf = 0
+    k_del = 0
+    for k in range(len(measurements)):
+
+        # opacity based on time
+        for measurement in measurements[k]:
+            alpha = 1 - max(min_alpha, 1 - measurement.t / end_time)
+            color = "r" if measurement.is_clutter else "k"
+            plt.scatter(
+                measurement.pos[0],
+                measurement.pos[1],
+                marker="x",
+                color=color,
+                alpha=alpha,
+            )
+
+        if estimate_status[k] == TRACK_STATUS.tentative_confirm:
+            for tentative_estimate in tentative_estimates[k_ten]:
+                #alpha = 1 - max(min_alpha, 1 - measurement.t / end_time)
+                color = "y"
+                plt.scatter(
+                    tentative_estimate[0],
+                    tentative_estimate[1],
+                    marker="+",
+                    color=color,
+                    #alpha=alpha,
+                )
+            k_ten += 1
+
+        if estimate_status[k] == TRACK_STATUS.confirmed:
+            estimates_at_t = conf_estimates[k_conf]
+            color = "g" 
+            plt.scatter(
+                estimates_at_t[0],
+                estimates_at_t[1],
+                marker="+",
+                color=color,
+                # alpha=alpha,
+            )
+            k_conf += 1
+
+        if estimate_status[k] == TRACK_STATUS.tentative_delete:
+            del_estimates_at_t = tentative_del_estimates[k_del]
+            color = "r" 
+            plt.scatter(
+                del_estimates_at_t[0],
+                del_estimates_at_t[1],
+                marker="+",
+                color=color,
+                # alpha=alpha,
+            )
+            k_del += 1
+
+        plt.draw()
+        if wait_for_btn_press:
+            plt.waitforbuttonpress()
+        else:
+            plt.pause(0.1)
+
+    plt.waitforbuttonpress()
+
+
+
+
 
 
 # def plot_pos_and_vel(estimates):
--- navigation/tracking/scripts/test/test_track_mangager.py
+++ navigation/tracking/scripts/test/test_track_mangager.py
@@ -100,6 +100,68 @@ def test_tentative_confirm_del():
     plots.plot_tentative_confirm_del(scenario, measurements, ground_truths, tentative_estimates, conf_estimates, tentative_del_estimates)
 
 
+def test_plot_interactive():
+
+    wait_for_btn_press = True
+
+    manager = TRACK_MANAGER()
+
+    manager.N = 3
+    manager.M = 8
+
+    scenario, measurements, ground_truths = data_generation()
+    
+    manager.main_track.p_no_match = 1 - scenario.config.probability.detection    
+    manager.main_track.time_step = scenario.config.dt
+
+    manager.sd = scenario.config.noise.measurement
+
+    for i in range(len(manager.main_track.state_post)):
+        manager.main_track.Q[i, i] = scenario.config.noise.process
+
+    for i in range(len(manager.main_track.C)):
+        manager.main_track.R[i, i] = scenario.config.noise.measurement
+
+    manager.max_vel = 3
+
+    tentative_estimates = []
+    conf_estimates = []
+    tentative_del_estimates = []
+    estimate_status = []
+
+    for o_time_k in measurements:
+
+        o_list = []
+        for o in o_time_k:
+            o_list.append(o.pos)
+        o_arr = np.array(o_list)
+
+        #update
+        manager.cb(o_arr)
+
+        #add updates to lists that will be plottee
+        if manager.main_track_status == TRACK_STATUS.tentative_confirm:
+            last_addition_to_tentative_tracks = []
+            for track in manager.tentative_tracks:
+                last_addition_to_tentative_tracks.append(track.state_post[:2])
+            
+            tentative_estimates.append(last_addition_to_tentative_tracks)
+
+        if manager.main_track_status == TRACK_STATUS.confirmed:
+            conf_estimates.append(manager.main_track.state_post[:2])
+        
+        if manager.main_track_status == TRACK_STATUS.tentative_delete:
+            tentative_del_estimates.append(manager.main_track.state_post[:2])
+
+        estimate_status.append(manager.main_track_status)
+
+    #print(estimate_status)
+
+    plots.plot_interactive(scenario, measurements, ground_truths, tentative_estimates, conf_estimates, tentative_del_estimates, estimate_status, wait_for_btn_press)
+
+
+
+
 
 
 
