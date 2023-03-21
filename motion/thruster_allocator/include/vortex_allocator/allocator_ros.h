73d454d79503710145c15cae1933c8586cf7db92
BenG_and_RK <vortex@vortexntnu.no>
2023-03-21 22:46:55 +0100

    Fixed cfgs, TAM, and allocator

--- motion/thruster_allocator/include/vortex_allocator/allocator_ros.h
+++ motion/thruster_allocator/include/vortex_allocator/allocator_ros.h
@@ -14,29 +14,50 @@ class Allocator {
 public:
   explicit Allocator(ros::NodeHandle nh);
   /**
-   * @brief Callback function for converting force wrench in body frame to
-   * ThrusterForces msg
+   * @brief Callback function for getting force wrench
    *
-   * @param msg Wrench message containing linear and angular forces in BODY
+   * @param msg Wrench message containing linear forces in BODY
    * frame
    */
-  void forceWrenchCallback(const geometry_msgs::Wrench &msg) const;
+  void forceWrenchCallback(const geometry_msgs::Wrench &msg);
+
+  /**
+   * @brief Callback function for getting torque wrench
+   *
+   * @param msg Wrench message containing angular forces in BODY
+   * frame
+   */
+  void torqueWrenchCallback(const geometry_msgs::Wrench &msg);
+
+  /**
+   * @brief Function for periodically converting force & torque wrench in body frame to
+   * ThrusterForces msg
+   *
+   */
+  void spinOnce();
 
 private:
   ros::NodeHandle m_nh;
-  ros::Subscriber m_sub;
+  ros::Subscriber m_sub_torque;
+  ros::Subscriber m_sub_force;
   ros::Publisher m_pub;
 
   int m_num_degrees_of_freedom;
   int m_num_thrusters;
+
   std::vector<int> m_direction;
   double m_min_thrust;
   double m_max_thrust;
   const double c_force_range_limit = 100;
 
+  float body_frame_force_x;
+  float body_frame_force_y;
+  float body_frame_torque;
+
   std::unique_ptr<PseudoinverseAllocator> m_pseudoinverse_allocator;
 
-  Eigen::VectorXd WrenchMsgToEigen(const geometry_msgs::Wrench &msg) const;
+  Eigen::VectorXd wrenchMsgToEigen(const geometry_msgs::Wrench &msg) const;
+  Eigen::VectorXd wrenchMsgToEigen(const float force_x, const float force_y, const float torque) const;
   bool healthyWrench(const Eigen::VectorXd &v) const;
 };
 
