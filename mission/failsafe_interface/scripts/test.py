20c2a000842b8dc445cd00df1d5a231f8482959e
Theo Martin <theoam@stud.ntnu.no>
2023-04-04 15:49:41 +0200

    Changed test script

--- mission/failsafe_interface/scripts/test.py
+++ mission/failsafe_interface/scripts/test.py
@@ -1,17 +1,22 @@
 import RPi.GPIO as GPIO
 
 if __name__ == "__main__":
-    inPIN = 20
-    outPIN = 21
+    outPIN = 11
+    inPIN = 13
+    sigPIN = 15
 
     GPIO.setmode(GPIO.BCM)  # Use BCM numbering
 
     GPIO.setup(inPIN, GPIO.IN)  # Set pin 4 as input
     GPIO.setup(outPIN, GPIO.OUT)  # Set pin 4 as input
+    GPIO.setup(sigPIN, GPIO.OUT)
 
-    inSig = GPIO.input(inPIN)
+    GPIO.output(outPIN, 1)
 
-    GPIO.output(outPIN, inSig)
+    while(True):
+        inSig = GPIO.input(inPIN)
 
-    print("Input: ", inSig)
+        GPIO.output(sigPIN, inSig)
+
+        print("Input: ", inSig)
 
