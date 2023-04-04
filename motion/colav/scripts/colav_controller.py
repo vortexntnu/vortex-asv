85b4a96fcf6a3d0a2f031001f6933482f8ed38df
Vortex HP <post@vortexntnu.no>
2023-04-04 15:17:54 +0200

    First draft

--- motion/colav/scripts/colav_controller.py
+++ motion/colav/scripts/colav_controller.py
@@ -1,10 +1,17 @@
 
+#!/usr/bin/python3
+
+
+
 import rospy
 from nav_msgs.msg import Odometry
 from vortex_msgs.msg import GuidanceData
 from enum import Enum
 import math
-from VOMATH import VelocityObstacle
+from VOMATH import VelocityObstacle,Obstacle,Zones,Approaches
+from tf.transformations import euler_from_quaternion
+
+
 
 
 #----------------------------------------------------
@@ -12,27 +19,28 @@ from VOMATH import VelocityObstacle
 # so just assuming odometry or now
 #----------------------------------------------------
 
-class Zones(Enum):
-    NOCOL = 1
-    COLIMM = 2
-    STOPNOW = 3
+# class Zones(Enum):
+#     NOCOL = 1
+#     COLIMM = 2
+#     STOPNOW = 3
 
 
-class Approaches(Enum):
-    FRONT = 1
-    RIGHT = 2
-    LEFT = 3
-    BEHIND = 4
+# class Approaches(Enum):
+#     FRONT = 1
+#     RIGHT = 2
+#     LEFT = 3
+#     BEHIND = 4
 
 
-class Obstacle:
-    def __init__(self) -> None:
-        self.vx = 0
-        self.vy = 0
-        self.r = 0
-        self.x = 0
-        self.y = 0
-        self.heading = 0
+# class Obstacle:
+#     def __init__(self) -> None:
+#         self.vx = 0
+#         self.vy = 0
+#         self.r = 0
+#         self.x = 0
+#         self.y = 0
+#         self.heading = 0
+#         self.speed = 0
 
 
 class ColavController:
@@ -77,7 +85,6 @@ class ColavController:
 
 
     def get_closest_obst(self,obstacles:dict) -> Odometry:
-        obst_list = []
         shortest_dist = math.inf
         closest_obst = None
         for obstacle in obstacles.items:
@@ -105,21 +112,90 @@ class ColavController:
         converted.y = obstacle.pose.pose.position.y
         converted.vx = obstacle.twist.twist.linear.x
         converted.vy = obstacle.twist.twist.linear.y
+        converted.heading = math.atan2(converted.vy,converted.vx)
+        converted.speed = math.sqrt(converted.vx**2+converted.vy**2)
         return converted
 
     def gen_colav_data(self):
         closest_obst = self.get_closest_obst(self.obstacles)
-
         VO = VelocityObstacle(self.vessel,closest_obst)
+        VO.set_cone_angles()
         
+        zone = self.get_zone(closest_obst,self.vessel)
+        if zone == Zones.NOCOL:
+            return None
+        elif zone == Zones.STOPNOW:
+            data = GuidanceData()
+            data.psi_d = self.vessel.heading
+            data.u_d = 0
+            data.u = 0
+            data.t = rospy.Time.now()
+            orientation_list = [
+                self.vessel.pose.pose.orientation.x,
+                self.vessel.pose.pose.orientation.y,
+                self.vessel.pose.pose.orientation.z,
+                self.vessel.pose.pose.orientation.w,
+            ]
+            data.psi = euler_from_quaternion(orientation_list)[2]
+            ##Set velocity to zero and heading to same as before:))
+            return data
+        elif zone == Zones.COLIMM and not VO.check_if_collision():
+            return None
         
-
-
-
-
+        #Now in collision imminent state
         
+        approach = self.gen_approach(closest_obst,self.vessel)
+
+        if approach == Approaches.FRONT or approach == Approaches.RIGHT:
+
+            buffer = math.pi/6
+            new_heading = VO.right_angle+ buffer
+
+            data = GuidanceData()
+            data.psi_d = new_heading
+            data.u_d = self.vessel.speed
+            data.u = self.vessel.speed
+            data.t = rospy.Time.now()
+            orientation_list = [
+                self.vessel.pose.pose.orientation.x,
+                self.vessel.pose.pose.orientation.y,
+                self.vessel.pose.pose.orientation.z,
+                self.vessel.pose.pose.orientation.w,
+            ]
+            data.psi = euler_from_quaternion(orientation_list)[2]
+            # choose right cone
+            return data
+        elif approach == Approaches.BEHIND or approach == Approaches.LEFT:
+            return None
+        return None
 
 
+
+    def gen_approach(obstacle:Obstacle,vessel : Obstacle) -> Approaches:
+        dx = obstacle.x-vessel.x
+        dy = obstacle.y-vessel.y
+        buffer = 10 * math.pi/180
+        phi = math.atan2(dy,dx)
+
+        if vessel.heading + buffer > phi and vessel.heading -buffer < phi:
+            return Approaches.FRONT
+        elif vessel.heading - buffer >phi:
+            return Approaches.RIGHT
+        elif vessel.heading + buffer < phi:
+            return Approaches.LEFT
+        return  Approaches.BEHIND
+             
+
+
+
+
+    def get_zone(self,obstacle : Obstacle, vessel:Obstacle) -> Zones:
+        distance = self.get_distance(obstacle,vessel)
+        if distance > self.colimm_max_r:
+            return Zones.NOCOL
+        elif distance < self.colimm_max_r and distance > self.stop_zone_r:
+            return Zones.COLIMM
+        return Zones.STOPNOW
     def run(self):
         while True:
             if rospy.get_param("/tasks/task_1"):
