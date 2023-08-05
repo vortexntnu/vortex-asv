d2018bb6abeed091466af8da66eb8674349b2d50
Clang Robot <robot@example.com>
2023-08-05 23:29:17 +0000

    Committing clang-format changes

--- motion/thruster_interface/src/thruster_interface_ros.cpp
+++ motion/thruster_interface/src/thruster_interface_ros.cpp
@@ -28,7 +28,8 @@ void ThrusterInterfaceROS::thrustCallback(
   std::vector<int> thruster_map = {0, 1, 2, 3};
   std::vector<int> pwm_offsets = {100, 100, 100, 100};
   for (int i = 0; i < 4; i++) {
-    int pwm_clamped = std::min(std::max(pwm_values[i] + pwm_offsets[i], 1100), 1900);
+    int pwm_clamped =
+        std::min(std::max(pwm_values[i] + pwm_offsets[i], 1100), 1900);
     pwm_msg.positive_width_us.push_back();
     pwm_msg.pins.push_back(thruster_map[i]);
   }
