c77dc6f4cdc01e763fcbcf306d0670695dadbb81
Aldokan <aleksos@ntnu.no>
2023-09-17 15:43:22 +0200

    Created 2d wrench message function

---
+++ mission/joystick_interface/scripts/test/test_joystick_interface.py
@@ -0,0 +1,9 @@
+from scripts.joystick_interface import JoystickInterface
+from geometry_msgs.msg import Wrench
+
+class TestJoystickInterface:
+    def test_2d_wrench_msg(self):
+        msg = JoystickInterface().create_2d_wrench_message(2.0, 3.0, 4.0)
+        assert msg.force.x == 2.0
+        assert msg.force.y == 3.0
+        assert msg.torque.z == 4.0
