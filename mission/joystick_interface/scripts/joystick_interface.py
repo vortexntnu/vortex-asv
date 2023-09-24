ebf9edcb9d0858f51fe94dbd44a9b8dd4db9565e
Aldokan <aleksos@ntnu.no>
2023-09-24 14:14:35 +0200

    Created a joystick subscriber

--- mission/joystick_interface/scripts/joystick_interface.py
+++ mission/joystick_interface/scripts/joystick_interface.py
@@ -1,10 +1,46 @@
+import rclpy
+from rclpy.node import Node
 from geometry_msgs.msg import Wrench
+from sensor_msgs.msg import Joy
+#git remote set-url origin git@github.com:vortexntnu/vortex-asv.git
 
 
-class JoystickInterface:
+class JoystickInterface(Node):
 
     def __init__(self):
-        self.wrench_pub
+        #rclpy.init()
+        super().__init__('joystick_interface_node')
+        #Mapping copy pasted from the original file
+        self.joystick_buttons_map = [
+            "A",
+            "B",
+            "X",
+            "Y",
+            "LB",
+            "RB",
+            "back",
+            "start",
+            "power",
+            "stick_button_left",
+            "stick_button_right",
+        ]
+
+        self.joystick_axes_map = [
+            "horizontal_axis_left_stick",
+            "vertical_axis_left_stick",
+            "LT",
+            "horizontal_axis_right_stick",
+            "vertical_axis_right_stick",
+            "RT",
+            "dpad_horizontal",
+            "dpad_vertical",
+        ]
+
+        #self.wrench_publisher = self.create_publisher(Wrench, 'wrench_topic', 1)
+
+        #self.node = rclpy.create_node('joystick_interface')
+        self.subscriber = self.create_subscription(Joy, 'joy',self.joystick_cb, 1)
+        #rclpy.shutdown()
 
     def create_2d_wrench_message(self, x, y, yaw):
         wrench_msg = Wrench()
@@ -12,3 +48,23 @@ class JoystickInterface:
         wrench_msg.force.y = y
         wrench_msg.torque.z = yaw
         return wrench_msg
+    
+    
+    """"
+    def publish_wrench_message(self, wrench):
+        self.wrench.pub.publish(wrench)
+
+    """
+
+    def joystick_cb(self, msg):
+        #print(f"Received message: {msg.buttons}")
+        a = msg.buttons
+        return a
+
+    def main(args=None):
+        rclpy.init(args=args)
+        joystick_interface = JoystickInterface()
+        rclpy.spin(joystick_interface)
+        joystick_interface.destroy_node()
+        rclpy.shutdown()
+        return
--- mission/joystick_interface/scripts/test/test_joystick_interface.py
+++ mission/joystick_interface/scripts/test/test_joystick_interface.py
@@ -1,11 +1,44 @@
 from scripts.joystick_interface import JoystickInterface
+import rclpy
+from rclpy.node import Node
 from geometry_msgs.msg import Wrench
+from sensor_msgs.msg import Joy
+from sensor_msgs.msg import Joy
+
+
 
 
 class TestJoystickInterface:
+    def start(self):
+        rclpy.init()
+        j = JoystickInterface()
+        rclpy.shutdown()
 
     def test_2d_wrench_msg(self):
+        self.start()
         msg = JoystickInterface().create_2d_wrench_message(2.0, 3.0, 4.0)
         assert msg.force.x == 2.0
         assert msg.force.y == 3.0
         assert msg.torque.z == 4.0
+
+    #def test_publish_wrench_msg(self):
+        #msg = JoystickInterface().create_2d_wrench_message(2.0, 3.0, 4.0)
+        #publisher = JoystickInterface().publish_wrench_message(msg)
+        #assert self.get_logger() == msg
+        
+    #def test_joystick_input_output(self):
+    #    return
+    #
+    #
+    def test_suscriber(self):
+        self.start()
+        #subscriber = JoystickInterface().subscriber
+        #joystick = JoystickInterface()
+        joy_msg = Joy()
+        joy_msg.axes = [0.1, 0.2, 0.3]  # Customize axes values as needed
+        joy_msg.buttons = [1, 0, 1] 
+        #JoystickInterface().joystick_cb(joy_msg, subscriber)
+        #assert JoystickInterface().node.joy_msg
+        assert JoystickInterface().joystick_cb(joy_msg) == joy_msg.buttons
+    
+    
