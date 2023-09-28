ced1213292ea05fae8b828046e87eb6c0183f057
Aldokan <114017858+Aldokan@users.noreply.github.com>
2023-09-28 18:25:22 +0200

    Added a unit test for the wrench message publisher

--- mission/joystick_interface/scripts/test/test_joystick_interface.py
+++ mission/joystick_interface/scripts/test/test_joystick_interface.py
@@ -9,16 +9,22 @@ from sensor_msgs.msg import Joy
 class TestJoystickInterface:
 
     def test_2d_wrench_msg(self):
-        #self.start()
         msg = JoystickInterface().create_2d_wrench_message(2.0, 3.0, 4.0)
         assert msg.force.x == 2.0
         assert msg.force.y == 3.0
         assert msg.torque.z == 4.0
         rclpy.shutdown()
 
-    def test_suscriber(self):
+    def test_input_from_controller_into_wrench_msg(self):
         joy_msg = Joy()
-        joy_msg.axes = [0.1, 0.2, 0.3]
-        joy_msg.buttons = [1, 0, 1]
-        assert JoystickInterface().joystick_cb(joy_msg) == joy_msg.buttons
+        joy_msg.axes = [-1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]#Should have 8 inputs self.joystick_axes_map
+        joy_msg.buttons = [1, -1, -1, 0, 0, 6, 0, 0, 0, 0, 0]#Should have 11 inputs self.joystick_buttons_map 
+        wrench_msg = JoystickInterface().joystick_cb(joy_msg)
+        assert wrench_msg.force.x == -100.0
+        assert wrench_msg.force.y == -100.0
+        assert wrench_msg.torque.z == 0.0
         rclpy.shutdown()
+    
+
+
+    
