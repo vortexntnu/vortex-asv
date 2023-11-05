7e23e4f0cddefe2f0523eac8aff7d59a36deb8d6
RoseKapps <rose.j.kapps@gmail.com>
2023-11-05 17:53:50 +0100

    tried to fix issues with the i2c...

--- sensors/temperature/temperature/temperature_lib.py
+++ sensors/temperature/temperature/temperature_lib.py
@@ -8,7 +8,7 @@ class TemperatureModule:
 
         # Parameters
         # to read temperature from arduino through I2C
-        self.i2c_adress = 0x08  
+        self.i2c_address = 0x08  
 
         # init of I2C bus communication
         self.bus = smbus.SMBus(1) #was 1 before
@@ -23,11 +23,20 @@ class TemperatureModule:
             #print(str(self.bus.read_byte_data(self.i2c_adress, 1)))
             #system_temperature = "here is the temperature"
             #system_temperature = str(self.bus.read_byte_data(self.i2c_adress, 1))
+
+
+            #for i in range(100):
+             #   data_received = self.bus.read_i2c_block_data(self.i2c_address, i, 16)
+              #  print(f"Data received from register {i}: {data_received}")
+
+
+            data_received = self.bus.read_i2c_block_data(self.i2c_address, 0, 11)  # Read 16 bytes from address 0
             
-            data_received = self.bus.read_i2c_block_data(self.i2c_adress, 0, 18)  # Read 16 bytes from address 0
+            #read_i2c_block_data(i2c_addr, register, length, force=None) --> what register??
+
+            print(data_received)
+
             string_received = ''.join(chr(i) for i in data_received).strip('\x00')  
-
-
 
             return string_received
 
