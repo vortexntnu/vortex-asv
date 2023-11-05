981d382abe612acd343e88418b7cd3847b25f69e
Clang Robot <robot@example.com>
2023-11-05 10:57:29 +0000

    Committing clang-format changes

--- motion/thruster_allocator/test/allocator_test.cpp
+++ motion/thruster_allocator/test/allocator_test.cpp
@@ -1,83 +1,82 @@
+#include <geometry_msgs/msg/wrench.hpp>
 #include <gtest/gtest.h>
 #include <rclcpp/rclcpp.hpp>
 #include <thruster_allocator/allocator_ros.hpp>
 #include <vortex_msgs/msg/thruster_forces.hpp>
-#include <geometry_msgs/msg/wrench.hpp>
 
 #include <chrono>
 #include <functional>
 
 class AllocatorTest : public testing::Test, public rclcpp::Node {
-    protected:
-    //  std::shared_ptr<ThrusterAllocator> node_;
-    public:
-      AllocatorTest() : Node("AllocatorTest_node") {
-        subscription_ = this->create_subscription<vortex_msgs::msg::ThrusterForces>("thrust/thruster_forces", 1,
-          std::bind(&AllocatorTest::thruster_forces_callback, this, std::placeholders::_1));
+protected:
+  //  std::shared_ptr<ThrusterAllocator> node_;
+public:
+  AllocatorTest() : Node("AllocatorTest_node") {
+    subscription_ = this->create_subscription<vortex_msgs::msg::ThrusterForces>(
+        "thrust/thruster_forces", 1,
+        std::bind(&AllocatorTest::thruster_forces_callback, this,
+                  std::placeholders::_1));
 
-        publisher_ = this->create_publisher<geometry_msgs::msg::Wrench>(
-          "thrust/wrench_input", 1);
+    publisher_ = this->create_publisher<geometry_msgs::msg::Wrench>(
+        "thrust/wrench_input", 1);
 
-        timer_ = this->create_wall_timer(
-          100ms, std::bind(&AllocatorTest::timer_callback, this));
+    timer_ = this->create_wall_timer(
+        100ms, std::bind(&AllocatorTest::timer_callback, this));
 
-        message_received = false;
-        }
-      std::vector<double> thrust;
-      const double MAX_ERROR = 1e-4;
-      
-      void SetUp() override {
-        // rclcpp::spin(node_);
-        while(!isNodeReady()){
-          timer_callback();
-        }
-      }
+    message_received = false;
+  }
+  std::vector<double> thrust;
+  const double MAX_ERROR = 1e-4;
 
-      void Publish(double surge, double sway, double yaw) {
-        geometry_msgs::msg::Wrench msg;
-        msg.force.x = surge;
-        msg.force.y = sway;
-        msg.torque.z = yaw;
-        publisher_->publish(msg);
-      } 
+  void SetUp() override {
+    // rclcpp::spin(node_);
+    while (!isNodeReady()) {
+      timer_callback();
+    }
+  }
 
-      void ExpectThrustNear(double *arr) {
-        for (int i = 0; i < static_cast<int>(thrust.size()); ++i)
-        EXPECT_NEAR(thrust[i], arr[i], MAX_ERROR);
-      }
+  void Publish(double surge, double sway, double yaw) {
+    geometry_msgs::msg::Wrench msg;
+    msg.force.x = surge;
+    msg.force.y = sway;
+    msg.torque.z = yaw;
+    publisher_->publish(msg);
+  }
 
-      void WaitForMessage() {
-        while (!message_received)
-          timer_callback();
-      }
+  void ExpectThrustNear(double *arr) {
+    for (int i = 0; i < static_cast<int>(thrust.size()); ++i)
+      EXPECT_NEAR(thrust[i], arr[i], MAX_ERROR);
+  }
 
-    private:
-      rclcpp::Publisher<geometry_msgs::msg::Wrench>::SharedPtr publisher_;
-      rclcpp::Subscription<vortex_msgs::msg::ThrusterForces>::SharedPtr subscription_;
-      rclcpp::TimerBase::SharedPtr timer_;
-      bool message_received;
-      void thruster_forces_callback(const vortex_msgs::msg::ThrusterForces &thruster_msg) {
-        thrust = thruster_msg.thrust;
-        message_received = true;
-      };
-      void timer_callback() {
-      std::cout << "hello!" << std::endl;
-      };
+  void WaitForMessage() {
+    while (!message_received)
+      timer_callback();
+  }
 
-      bool isNodeReady() {
-        return (count_subscribers("thrust/thruster_forces") > 0) && (count_publishers("thrust/wrench_input") > 0);
-      }
+private:
+  rclcpp::Publisher<geometry_msgs::msg::Wrench>::SharedPtr publisher_;
+  rclcpp::Subscription<vortex_msgs::msg::ThrusterForces>::SharedPtr
+      subscription_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  bool message_received;
+  void thruster_forces_callback(
+      const vortex_msgs::msg::ThrusterForces &thruster_msg) {
+    thrust = thruster_msg.thrust;
+    message_received = true;
+  };
+  void timer_callback() { std::cout << "hello!" << std::endl; };
 
+  bool isNodeReady() {
+    return (count_subscribers("thrust/thruster_forces") > 0) &&
+           (count_publishers("thrust/wrench_input") > 0);
+  }
 };
 
-
-TEST_F(AllocatorTest, DumbTest) {
-  ASSERT_EQ(1, 1);
-}
+TEST_F(AllocatorTest, DumbTest) { ASSERT_EQ(1, 1); }
 
 // TEST_F(AllocatorTest, CheckResponsiveness) {
-//   Publish(0, 0, 0); 
-//   WaitForMessage(); 
+//   Publish(0, 0, 0);
+//   WaitForMessage();
 // }
 
 // TEST_F(AllocatorTest, ZeroInput) {
@@ -100,4 +99,3 @@ TEST_F(AllocatorTest, DumbTest) {
 //   rclcpp::spin_some(allocator_test);
 //   ASSERT_EQ(1, 1); // Example assertion
 // }
-
--- motion/thruster_allocator/test/main.cpp
+++ motion/thruster_allocator/test/main.cpp
@@ -1,8 +1,7 @@
 #include <gtest/gtest.h>
 #include <rclcpp/rclcpp.hpp>
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
   testing::InitGoogleTest(&argc, argv);
   int result = RUN_ALL_TESTS();
