91a04518f92680cde02ae37111e1ba46406f00ff
RoseKapps <rose.j.kapps@gmail.com>
2023-11-12 20:45:50 +0100

    Added launch file

--- sensors/temperature/setup.py
+++ sensors/temperature/setup.py
@@ -1,22 +1,25 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'temperature'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='vortex',
-    maintainer_email='rose.j.kapps@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer_email='karate.martynas1@gmail.com',
+    description='Temperature sensor data gartehring from temperature sensor system',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
