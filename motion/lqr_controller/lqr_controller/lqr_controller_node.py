00e8f565fbc674e80328aa669818d5aa84b9a3c1
vortex <vortex.git@vortexntnu.no>
2024-01-08 15:47:02 +0100

    Added dampening D

--- motion/lqr_controller/lqr_controller/lqr_controller_node.py
+++ motion/lqr_controller/lqr_controller/lqr_controller_node.py
@@ -10,17 +10,21 @@ class LQRControllerNode(Node):
             namespace='',
             parameters=[
                 ('lqr_controller.mass', 1.0),
+                ('lqr_controller.D', [0.0, 0.0, 0.0]),
                 ('lqr_controller.Q', [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
                 ('lqr_controller.R', [0.0, 0.0, 0.0])
             ])
 
         m = self.get_parameter('lqr_controller.mass').get_parameter_value().double_value
+        D = self.get_parameter('lqr_controller.D').get_parameter_value().double_array_value
         Q = self.get_parameter('lqr_controller.Q').get_parameter_value().double_array_value
         R = self.get_parameter('lqr_controller.R').get_parameter_value().double_array_value
 
         self.get_logger().info(f"Mass: {m}")
+        self.get_logger().info(f"D: {D}")
         self.get_logger().info(f"Q: {Q}")
         self.get_logger().info(f"R: {R}")
+
         self.get_logger().info("lqr_controller_node started")
 
 def main(args=None):
