d682df844a87639fb360eeac060ea8ea8b92b767
jorgenfj <jorgfje@stud.ntnu.no>
2024-01-08 16:28:17 +0100

    basic publishers run, working on velocity visualization

--- mission/landmarks/include/landmarks/landmarks.hpp
+++ mission/landmarks/include/landmarks/landmarks.hpp
@@ -2,30 +2,37 @@
 #define LANDMARKS_HPP
 
 #include <rclcpp/rclcpp.hpp>
-#include <vortex_msgs/msg/Landmark.msg>
-// #include <vortex_msgs/msg/LandmarkArray.msg>
-#include <nav_msgs/msg/OccupancyGrid.msg>
+#include <nav_msgs/msg/occupancy_grid.hpp>
+#include <vortex_msgs/msg/landmark.hpp>
+#include <vortex_msgs/msg/landmark_array.hpp>
 #include <vector>  
+#include <geometry_msgs/msg/pose_array.hpp>
+#include <geometry_msgs/msg/twist_stamped.hpp>
 
 namespace landmarks {
 
 class LandmarksNode : public rclcpp::Node {
 public:
-    LandmarksNode(){};
+    explicit LandmarksNode(const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
 
     ~LandmarksNode() {};
 
 protected:
-    rclcpp::Subscription<std::vector<vortex_msgs::msg::Landmark>>::SharedPtr subscription_;
-    void landmarksRecievedCallback(const std::vector<vortex_msgs::msg::Landmark::SharedPtr> msg);
-    rclcpp::Publisher<std::vector<vortex_msgs::msg::Landmark>::SharedPtr> landmarkPublisher_;
+    rclcpp::Subscription<vortex_msgs::msg::LandmarkArray>::SharedPtr subscription_;
+    void landmarksRecievedCallback(const vortex_msgs::msg::LandmarkArray::SharedPtr msg);
+    rclcpp::Publisher<vortex_msgs::msg::LandmarkArray>::SharedPtr landmarkPublisher_;
     rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr gridPublisher_;
-    std::vector<vortex_msgs::msg::Landmark> storedLandmarks_;
-    nav_msgs::msg::OccupancyGrid createGrid(std::vector<vortex_msgs::msg::Landmark> landmarks);
+    rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr posePublisher_;
+    rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twistPublisher_;
+    std::shared_ptr<vortex_msgs::msg::LandmarkArray> storedLandmarks_;
+    nav_msgs::msg::OccupancyGrid createGrid(vortex_msgs::msg::LandmarkArray landmarks);
     nav_msgs::msg::OccupancyGrid grid_;
     // maybe use timer based publishing?
     // void publishLandmarks();
-    void updateGrid(vortex_msgs::msg::Landmark landmark,uint8 number);
+    void updateGrid(vortex_msgs::msg::Landmark landmark,int number);
+    geometry_msgs::msg::PoseArray poseArrayCreater(vortex_msgs::msg::LandmarkArray landmarks);
+    void twistCreater(vortex_msgs::msg::LandmarkArray landmarks);
+    
 };
 
 }  // namespace landmarks
--- mission/landmarks/src/landmarks.cpp
+++ mission/landmarks/src/landmarks.cpp
@@ -4,25 +4,28 @@ using std::placeholders::_1;
 
 namespace landmarks {
 
-LandmarksNode::LandmarksNode() : Node("landmarks_node") {
 
-  LandmarksNode::LandmarksNode() : Node("landmarks_node") {
+  LandmarksNode::LandmarksNode(const rclcpp::NodeOptions& options) : Node("landmarks_node",options) {
     // Define the quality of service profile for publisher and subscriber
     rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
     qos_profile.reliability = RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT;
     auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 1),
                            qos_profile);
-    storedLandmarks_ = std::make_shared<vector<vortex_msgs::msg::Landmark>>();
+    storedLandmarks_ = std::make_shared<vortex_msgs::msg::LandmarkArray>();
     subscription_ =
-        this->create_subscription<vector<vortex_msgs::msg::Landmark>>(
+        this->create_subscription<vortex_msgs::msg::LandmarkArray>(
             "landmarks_in", qos,
             std::bind(&LandmarksNode::landmarksRecievedCallback, this, _1));
     landmarkPublisher_ =
-        this->create_publisher<vector<vortex_msgs::msg::Landmark>>(
+        this->create_publisher<vortex_msgs::msg::LandmarkArray>(
             "landmarks_out", qos);
     gridPublisher_ =
         this->create_publisher<nav_msgs::msg::OccupancyGrid>("grid_out", qos);
-    grid_ = createGrid(storedLandmarks_);
+    grid_ = createGrid(*storedLandmarks_);
+    posePublisher_ =
+        this->create_publisher<geometry_msgs::msg::PoseArray>("pose_out", qos);
+    twistPublisher_ =
+        this->create_publisher<geometry_msgs::msg::TwistStamped>("twist_out", qos);
 
     // Sets up a timer to trigger the publishMessages function every 1 second
     // timer_ = this->create_wall_timer(
@@ -30,49 +33,56 @@ LandmarksNode::LandmarksNode() : Node("landmarks_node") {
     //     std::bind(&LandmarksNode::publishLandmarks, this));
   }
 
+  
+
   void LandmarksNode::landmarksRecievedCallback(
-      const std::vector<vortex_msgs::msg::Landmark::SharedPtr> msg) {
-    for (const auto &landmark : msg) {
-      if (landmark.action == 0) {
-        for (int i = 0; i < storedLandmarks_.size(); i++) {
-          if (storedLandmarks_[i].id == landmark.id &&
-              storedLandmarks_[i].namespace == landmark.namespace) {
+    
+    const vortex_msgs::msg::LandmarkArray::SharedPtr msg) {
+    for (const auto &landmark : msg->landmarks) {
+      RCLCPP_INFO(this->get_logger(), "Landmarks received");
+        if (landmark.action == 0) {
+            for (int i = 0; i < storedLandmarks_->landmarks.size(); i++) {
+                if ((storedLandmarks_)->landmarks[i].id == landmark.id &&
+                    (storedLandmarks_)->landmarks[i].landmark_type == landmark.landmark_type) {
 
-            updateGrid(storedLandmarks_[i],0)
-            storedLandmarks_.erase(storedLandmarks_.begin() + i);
-                
-        }
-          }
-        }
-      } else if (landmark.action == 1) {
-        bool landmarkUpdated = false;
+                    updateGrid((storedLandmarks_->landmarks)[i], 0);
+                    (storedLandmarks_->landmarks).erase(storedLandmarks_->landmarks.begin() + i);
 
-        for (auto &storedLandmark : storedLandmarks_) {
-          if (incomingLandmark.namespace == storedLandmark->namespace &&
-              incomingLandmark.id == storedLandmark->id) {
-            // Update existing landmark if namespace and id match
-            updateGrid(storedLandmark,0)
-            updateGrid(incomingLandmark,1)
-            storedLandmark = incomingLandmark;
-            landmarkUpdated = true;
-            break;
-          }
-        }
+                    break;
+                }
+            }
+        } else if (landmark.action == 1) {
+            bool landmarkUpdated = false;
 
-        if (!landmarkUpdated && incomingLandmark.action == 1) {
-          // If no match is found and action is 1, add the new landmark
-          storedLandmarks_.push_back(incomingLandmark);
-          updateGrid(incomingLandmark,1)
+            for (auto &storedLandmark : storedLandmarks_->landmarks) {
+                if (landmark.landmark_type == storedLandmark.landmark_type &&
+                    landmark.id == storedLandmark.id) {
+                    // Update existing landmark if namespace and id match
+                    updateGrid(storedLandmark, 0);
+                    updateGrid(landmark, 1);
+                    storedLandmark = landmark;
+                    landmarkUpdated = true;
+                    break;
+                }
+            }
+
+            if (!landmarkUpdated && landmark.action == 1) {
+                // If no match is found and action is 1, add the new landmark
+                (storedLandmarks_->landmarks).push_back(landmark);
+                updateGrid(landmark, 1);
+            }
         }
-      }
-    //   publish after each landmark is updated or after the callback is done? Or just timer based?
-      landmarkPublisher_->publish(storedLandmarks_);
-      gridPublisher_->publish(createGrid(storedLandmarks_));
     }
-      }
+    //   publish after each landmark is updated or after the callback is done? Or just timer based?
+    landmarkPublisher_->publish(*storedLandmarks_);
+    gridPublisher_->publish(grid_);
+    posePublisher_->publish(poseArrayCreater(*storedLandmarks_));
+    twistCreater(*storedLandmarks_);
+}
+
 
     nav_msgs::msg::OccupancyGrid LandmarksNode::createGrid(
-        std::vector<vortex_msgs::msg::Landmark> landmarks) {
+        vortex_msgs::msg::LandmarkArray landmarks) {
       // Assuming some parameters for the occupancy grid
       int grid_width = 100;
       int grid_height = 100;
@@ -89,23 +99,49 @@ LandmarksNode::LandmarksNode() : Node("landmarks_node") {
       // Initialize the data array
       grid.data.resize(grid_width * grid_height,
                        0); // Initialize unoccupied grid
+        for(const auto &landmark : landmarks.landmarks){
+            updateGrid(landmark,1);
+        }
       return grid;
         }
 
 
-      void LandmarksNode::updateGrid(vortex_msgs::msg::Landmark landmark, uint8 number ){
+      void LandmarksNode::updateGrid(vortex_msgs::msg::Landmark landmark, int number ){
       // Update the data array based on landmark poses
-      for (const auto &landmark : landmarks) {
-        int x = landmark.pose.position.x / grid.info.resolution;
-        int y = landmark.pose.position.y / grid.info.resolution;
+    
+        int x = landmark.pose.position.x / grid_.info.resolution;
+        int y = landmark.pose.position.y / grid_.info.resolution;
 
         // Check bounds
-        if (x >= 0 && x < grid_width && y >= 0 && y < grid_height) {
-          grid.data[x + y * grid.info.width] = number;
+        if (x >= 0 && x < grid_.info.width && y >= 0 && y < grid_.info.height) {
+          grid_.data[x + y * grid_.info.width] = number;
         }
-      }
     
   }
 
+  geometry_msgs::msg::PoseArray LandmarksNode::poseArrayCreater(vortex_msgs::msg::LandmarkArray landmarks){
+      geometry_msgs::msg::PoseArray poseArray;
+      poseArray.header.stamp = this->get_clock()->now();
+      poseArray.header.frame_id = "pose_map";
+      for (const auto& landmark : landmarks.landmarks) {
+      // Convert landmark to pose here...
+        poseArray.poses.push_back(landmark.pose);
+        }
+        return poseArray;
+  }
+
+  void LandmarksNode::twistCreater(vortex_msgs::msg::LandmarkArray landmarks){
+     
+      for (auto landmark : landmarks.landmarks) {
+        landmark.twist.header.stamp = this->get_clock()->now();
+        landmark.twist.header.frame_id = "pose_map";
+      // Convert landmark to pose here...
+        twistPublisher_->publish(landmark.twist);
+        }
+  }
+
+// publiser pose of landmark som en liste med velocity, er bare for visualisering
+// fisk grid så flere kan okkupere samme celle
+// start med actions for filtrering
 
 } // namespace landmarks
