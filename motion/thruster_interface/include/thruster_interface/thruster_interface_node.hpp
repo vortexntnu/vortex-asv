de0b0e7bc1aa3e5e2bb7249d49ea89644bc88dd1
Clang Robot <robot@example.com>
2024-01-10 14:47:58 +0000

    Committing clang-format changes

--- motion/thruster_interface/include/thruster_interface/thruster_interface_node.hpp
+++ motion/thruster_interface/include/thruster_interface/thruster_interface_node.hpp
@@ -20,7 +20,7 @@ private:
   rclcpp::Publisher<vortex_msgs::msg::Pwm>::SharedPtr pwm_pub_;
   std::vector<int> thruster_to_pin_map_;
   std::vector<int> thruster_direction_map_;
-  std::vector<int> pwm_offsets_;  
+  std::vector<int> pwm_offsets_;
 
 public:
   ThrusterInterfaceROS() : Node("thruster_interface") {
@@ -29,13 +29,18 @@ public:
         this->create_subscription<vortex_msgs::msg::ThrusterForces>(
             "thrust/thruster_forces", 10,
             std::bind(&ThrusterInterfaceROS::thrust_callback, this, _1));
-  declare_parameter(thruster_to_pin_map_, {1, 3, 2, 0});
-  declare_parameter(thruster_direction_map_, {1, 1, 1, -1});
-  declare_parameter(pwm_offsets_, {100, 100, 100, 100});
-  thruster_to_pin_map_ = get_parameter("propulsion.thrusters.thruster_to_pin_map").as_integer_array();
-  thruster_direction_map_ = get_parameter("propulsion.thrusters.thruster_direction").as_integer_array();
-  pwm_offsets_ = get_parameter("propulsion.thrusters.pwm_offsets").as_integer_array();
+    declare_parameter(thruster_to_pin_map_, {1, 3, 2, 0});
+    declare_parameter(thruster_direction_map_, {1, 1, 1, -1});
+    declare_parameter(pwm_offsets_, {100, 100, 100, 100});
+    thruster_to_pin_map_ =
+        get_parameter("propulsion.thrusters.thruster_to_pin_map")
+            .as_integer_array();
+    thruster_direction_map_ =
+        get_parameter("propulsion.thrusters.thruster_direction")
+            .as_integer_array();
+    pwm_offsets_ =
+        get_parameter("propulsion.thrusters.pwm_offsets").as_integer_array();
   }
 
-  void thrust_callback(const vortex_msgs::msg::ThrusterForces::SharedPtr msg);  
+  void thrust_callback(const vortex_msgs::msg::ThrusterForces::SharedPtr msg);
 };
