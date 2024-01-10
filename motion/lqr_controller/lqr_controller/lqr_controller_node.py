091f6249e54f0ac6b95c88059e9ff97fbc3916c9
vortex <vortex.git@vortexntnu.no>
2024-01-10 17:45:24 +0100

    WIP need to fix lqr

--- motion/lqr_controller/lqr_controller/lqr_controller_node.py
+++ motion/lqr_controller/lqr_controller/lqr_controller_node.py
@@ -21,8 +21,8 @@ class LQRControllerNode(Node):
             ])
         
         self.wrench_publisher_ = self.create_publisher(Wrench, "thrust/wrench_input", 1)
-        self.state_subscriber_ = self.create_subscription(Odometry, "/sensor/seapath/odometry/ned", self.state_callback, 1)
-        self.guidance_subscriber_ = self.create_subscription(Odometry, "path/pathpath", self.guidance_callback, 1)
+        self.state_subscriber_ = self.create_subscription(Odometry, "/sensor/seapath/odometry/ned", self.state_cb, 1)
+        self.guidance_subscriber_ = self.create_subscription(Odometry, "controller/lqr/reference", self.guidance_cb, 1)
 
         m = self.get_parameter('lqr_controller.mass').get_parameter_value().double_value
         D = self.get_parameter('lqr_controller.D').get_parameter_value().double_array_value
@@ -41,33 +41,34 @@ class LQRControllerNode(Node):
 
         self.get_logger().info("lqr_controller_node started")
 
-
-    def guidance_callback(self, msg):
-        pass
-
-    def state_callback(self, msg):
-
+    def odometrymsg_to_state(self, msg):
         x = msg.pose.pose.position.x
         y = msg.pose.pose.position.y
-        
         orientation_q = msg.pose.pose.orientation
         orientation_list = [
             orientation_q.w, orientation_q.x, orientation_q.y, orientation_q.z
         ]
-        
-        # Convert quaternion to Euler angles
-        yaw = quat2euler(orientation_list)[2]
 
-        # Gjør en relinearisering
-        self.lqr_controller.linearize_model(yaw)
+        # Convert quaternion to Euler angles, extract yaw
+        yaw = quat2euler(orientation_list)[2]
 
         vx = msg.twist.twist.linear.x
         vy = msg.twist.twist.linear.y
         vyaw = msg.twist.twist.angular.z
 
         state = np.array([x, y, yaw, vx, vy, vyaw])
+        return state
+    
+    def guidance_cb(self, msg):
+        self.x_ref = self.odometrymsg_to_state(msg)
 
-        # Kjør LQR
+    def state_cb(self, msg):
+
+        state = self.odometrymsg_to_state(msg)
+
+        self.lqr_controller.linearize_model(state[2])
+
+        # Run LQR 
         u = self.lqr_controller.calculate_control_input(state, self.x_ref, self.lqr_controller.K_LQR, self.lqr_controller.K_r)
 
         wrench = Wrench()
@@ -78,9 +79,8 @@ class LQRControllerNode(Node):
         wrench.torque.y = 0.0
         wrench.torque.z = u[2]
 
-        #Publisher til thrust/wrench_input
+        # Publish thrust/wrench_input
         self.wrench_publisher_.publish(wrench)
-
 
 
 def main(args=None):
