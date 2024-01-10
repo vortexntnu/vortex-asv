793b35d27f4c17652c9c34c997265779b9008e36
Aldokan <aleksos@ntnu.no>
2024-01-10 16:01:43 +0100

    Removed direction mapping from thruster allocator

--- motion/thruster_allocator/src/allocator_ros.cpp
+++ motion/thruster_allocator/src/allocator_ros.cpp
@@ -15,7 +15,6 @@ ThrusterAllocator::ThrusterAllocator()
   declare_parameter("propulsion.thrusters.num", 4);
   declare_parameter("propulsion.thrusters.min", -100);
   declare_parameter("propulsion.thrusters.max", 100);
-  declare_parameter("propulsion.thrusters.direction", std::vector<int64_t>{0});
   declare_parameter("propulsion.thrusters.configuration_matrix",
                     std::vector<double>{0});
 
@@ -23,8 +22,6 @@ ThrusterAllocator::ThrusterAllocator()
   num_thrusters_ = get_parameter("propulsion.thrusters.num").as_int();
   min_thrust_ = get_parameter("propulsion.thrusters.min").as_int();
   max_thrust_ = get_parameter("propulsion.thrusters.max").as_int();
-  direction_ =
-      get_parameter("propulsion.thrusters.direction").as_integer_array();
   thrust_configuration = doubleArrayToEigenMatrix(
       get_parameter("propulsion.thrusters.configuration_matrix")
           .as_double_array(),
