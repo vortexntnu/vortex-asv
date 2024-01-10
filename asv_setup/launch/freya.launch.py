f0619336fe9bf746bac56668b56edae9ed4378ef
Aldokan <aleksos@ntnu.no>
2024-01-10 14:09:17 +0100

    cleanup

--- asv_setup/launch/freya.launch.py
+++ asv_setup/launch/freya.launch.py
@@ -20,6 +20,7 @@ def generate_launch_description():
         )
     )
 
+    #Thruster Interface launch
     thruster_interface_launch = IncludeLaunchDescription(
     PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('thruster_interface'), 'launch/thruster_interface.launch.py')
