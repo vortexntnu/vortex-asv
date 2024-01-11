f050c6489291c94f5fea93d1fe92e4208b32b102
vortex <vortex.git@vortexntnu.no>
2024-01-11 15:38:57 +0100

    Made package

---
+++ motion/vessel_simulator/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'vessel_simulator'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='vortex',
+    maintainer_email='vortex.git@vortexntnu.no',
+    description='TODO: Package description',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'vessel_simulator_node = vessel_simulator.vessel_simulator_node:main'
+        ],
+    },
+)
