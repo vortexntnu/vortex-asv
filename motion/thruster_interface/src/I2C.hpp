62231b46ca3c10bc538d94deab83ecf995c5c504
Clang Robot <robot@example.com>
2024-01-25 16:08:52 +0000

    Committing clang-format changes

--- motion/thruster_interface/src/I2C.hpp
+++ motion/thruster_interface/src/I2C.hpp
@@ -1,10 +1,9 @@
-#include <linux/i2c-dev.h>
-#include <iostream>
 #include <fcntl.h>
+#include <iostream>
+#include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <vector>
-
 
 void send_status(int8_t status, int file);
 std::vector<uint8_t> pwm_to_bytes(std::vector<uint16_t> pwm_values);
