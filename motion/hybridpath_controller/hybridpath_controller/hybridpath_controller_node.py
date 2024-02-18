dcef0819260f5490de45051bc9c8dc21c1db928f
Martin Huynh <martiniuss.wan@gmail.com>
2024-02-18 13:28:10 +0100

    Added simulation function

--- motion/hybridpath_controller/hybridpath_controller/hybridpath_controller_node.py
+++ motion/hybridpath_controller/hybridpath_controller/hybridpath_controller_node.py
@@ -9,10 +9,71 @@ class HybridPathControllerNode(Node):
     def __init__(self):
         super().__init__("hybridpath_controller_node")
         
-        self.get_logger().info("hybridpath_controller_node started")
+        self.get_logger().info("hybridpath_controller_node started")    
 
+def run_simulation(waypoints, T, dt):
+    lambda_val = 0.6  # Curvature constant
+    r = 2  # Differentiability order
+    generator = HybridPathGenerator(waypoints, r, lambda_val, 1)
+    path = generator.Path
+    N = path['NumSubpaths']
+    time = np.arange(0, T, dt)
 
-def plotAdaptiveBackstep(eta_d, eta, time, tau, nu):
+    # Initial vectors
+    u_0 = 0 # Initial speed
+    eta_d = np.zeros((3, len(time)))
+    eta = np.zeros((3, len(time)))
+    eta_d[:,0] = np.array([waypoints[0,0], waypoints[0,1], np.pi/2])
+    eta[:,0] = np.array([waypoints[0,0], waypoints[0,1], np.pi/2])
+    nu = np.zeros((3, len(time)))
+    tau = np.zeros((3, len(time)))
+    AB = AdaptiveBackstep()
+    s = 0
+    sig = HybridPathSignals(path, s)
+
+    # Simulation loop
+    for i in range(1,len(time)):
+        time_to_max_speed = 10
+        if time[i] < time_to_max_speed:
+            u_desired = 1/10 * time[i]
+        else:
+            u_desired = 1 # Desired speed
+        v_ref, v_ref_s = sig.calc_vs(u_desired)
+        s += v_ref * dt
+        sig = HybridPathSignals(path, s)
+        pd = sig.pd # Reference position
+        psi_d = sig.psi # Reference heading
+        eta_d[:,i] = np.array([pd[0], pd[1], psi_d])
+        psi0 = eta_d[2,i-1]
+        psi1 = eta_d[2,i]
+        psi_vec = np.array([psi0, psi1])
+        psi_vec = np.unwrap(psi_vec, period=2*np.pi)
+        eta_d[2,i] = psi_vec[1]
+
+        # Variables needed for tau
+        #w = 0
+        v_ref_t = 0 # Constant speed
+        eta_d_s = np.array([sig.pd_der[0][0], sig.pd_der[0][1], sig.psi_der])
+        eta_d_ss = np.array([sig.pd_der[1][0], sig.pd_der[1][1], sig.psi_dder])
+        R, R_trsp = AB.R(eta[2,i-1])
+        eta_error = eta[:,i-1] - eta_d[:,i]
+        my = 0.5
+        w = my * (eta_d_s @ R @ (R_trsp @ eta_error)) / (np.linalg.norm(eta_d_s)**2)
+
+        tau[:,i] = AB.control_law(eta[:,i-1], nu[:,i-1], w, v_ref, v_ref_t, v_ref_s, eta_d[:,i], eta_d_s, eta_d_ss)
+        
+        # Step in nu and eta
+        nu_dot = np.linalg.inv(AB.M) @ tau[:, i] - np.linalg.inv(AB.M) @ AB.D @ nu[:,i-1]
+        nu[:,i] = nu[:,i-1] + nu_dot * dt
+        eta_dot = R @ nu[:,i]
+        eta[:,i] = eta[:,i-1] + eta_dot * dt
+
+        psi0 = eta[2,i-1]
+        psi1 = eta[2,i]
+        psi_vec = np.array([psi0, psi1])
+        psi_vec = np.unwrap(psi_vec, period=2*np.pi)
+        eta[2,i] = psi_vec[1]
+    
     plt.figure()
     plt.plot(eta_d[1,:], eta_d[0,:], label='Reference path', zorder = 0)
     plt.plot(eta[1,:], eta[0,:], label='Actual path', zorder = 1)
@@ -70,8 +131,7 @@ def plotAdaptiveBackstep(eta_d, eta, time, tau, nu):
     plt.show()
 
 def main(args=None):
-    # Waypoints
-    pos = np.array([[10, 0],
+    waypoints = np.array([[10, 0],
             [10, 10],
             [0, 20],
             [30, 30],
@@ -79,74 +139,11 @@ def main(args=None):
             [0,-10],
             [0, 0],
             [10, 0]])
-
-    pos2 = np.array([[10, 0], [20, 10], [30, 20], [50, 20],[50, 80], [-20, 80]])
-
-    lambda_val = 0.6  # Curvature constant
-    r = 2  # Differentiability order
-    generator = HybridPathGenerator(pos, r, lambda_val, 1)
-    path = generator.Path
-    N = path['NumSubpaths']
+    
     T = 200
     dt = 0.05
-    time = np.arange(0, T, dt)
-
-    # Initial vectors
-    u_0 = 0 # Initial speed
-    eta_d = np.zeros((3, len(time)))
-    eta = np.zeros((3, len(time)))
-    eta_d[:,0] = np.array([pos[0,0], pos[0,1], np.pi/2])
-    eta[:,0] = np.array([pos[0,0], pos[0,1], np.pi/2])
-    nu = np.zeros((3, len(time)))
-    tau = np.zeros((3, len(time)))
-    AB = AdaptiveBackstep()
-    s = 0
-    sig = HybridPathSignals(path, s)
-
-    # Simulation loop
-    for i in range(1,len(time)):
-        time_to_max_speed = 10
-        if time[i] < time_to_max_speed:
-            u_desired = 1/10 * time[i]
-        else:
-            u_desired = 1 # Desired speed
-        v_ref, v_ref_s = sig.calc_vs(u_desired)
-        s += v_ref * dt
-        sig = HybridPathSignals(path, s)
-        pd = sig.pd # Reference position
-        psi_d = sig.psi # Reference heading
-        eta_d[:,i] = np.array([pd[0], pd[1], psi_d])
-        psi0 = eta_d[2,i-1]
-        psi1 = eta_d[2,i]
-        psi_vec = np.array([psi0, psi1])
-        psi_vec = np.unwrap(psi_vec, period=2*np.pi)
-        eta_d[2,i] = psi_vec[1]
-
-        # Variables needed for tau
-        #w = 0
-        v_ref_t = 0 # Constant speed
-        eta_d_s = np.array([sig.pd_der[0][0], sig.pd_der[0][1], sig.psi_der])
-        eta_d_ss = np.array([sig.pd_der[1][0], sig.pd_der[1][1], sig.psi_dder])
-        R, R_trsp = AB.R(eta[2,i-1])
-        eta_error = eta[:,i-1] - eta_d[:,i]
-        my = 0.5
-        w = my * (eta_d_s @ R @ (R_trsp @ eta_error)) / (np.linalg.norm(eta_d_s)**2)
-
-        tau[:,i] = AB.control_law(eta[:,i-1], nu[:,i-1], w, v_ref, v_ref_t, v_ref_s, eta_d[:,i], eta_d_s, eta_d_ss)
-        
-        # Step in nu and eta
-        nu_dot = np.linalg.inv(AB.M) @ tau[:, i] - np.linalg.inv(AB.M) @ AB.D @ nu[:,i-1]
-        nu[:,i] = nu[:,i-1] + nu_dot * dt
-        eta_dot = R @ nu[:,i]
-        eta[:,i] = eta[:,i-1] + eta_dot * dt
-
-        psi0 = eta[2,i-1]
-        psi1 = eta[2,i]
-        psi_vec = np.array([psi0, psi1])
-        psi_vec = np.unwrap(psi_vec, period=2*np.pi)
-        eta[2,i] = psi_vec[1]
-
-    plotAdaptiveBackstep(eta_d, eta, time, tau, nu)
+            
+    run_simulation(waypoints, T, dt)
 
     rclpy.init(args=args)
     node = HybridPathControllerNode()
