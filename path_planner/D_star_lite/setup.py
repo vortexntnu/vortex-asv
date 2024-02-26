4375cf197f8e1d72cd34c062c3008d49a8f49858
Andeshog <157955984+Andeshog@users.noreply.github.com>
2024-02-26 10:40:15 +0100

    initial commit

---
+++ path_planner/D_star_lite/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'D_star_lite'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='andeshog',
+    maintainer_email='157955984+Andeshog@users.noreply.github.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
