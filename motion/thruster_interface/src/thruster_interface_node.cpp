1111cc640d4d5cc26a7e14ae3ab9acc3c121acc9
Clang Robot <robot@example.com>
2024-02-29 17:48:14 +0000

    Committing clang-format changes

--- motion/thruster_interface/src/thruster_interface_node.cpp
+++ motion/thruster_interface/src/thruster_interface_node.cpp
@@ -4,26 +4,22 @@
 
 using std::placeholders::_1;
 
-class MinimalSubscriber : public rclcpp::Node
-{
+class MinimalSubscriber : public rclcpp::Node {
 public:
-  MinimalSubscriber()
-  : Node("minimal_subscriber")
-  {
+  MinimalSubscriber() : Node("minimal_subscriber") {
     subscription_ = this->create_subscription<std_msgs::msg::Float32>(
-      "float_topic", 10, std::bind(&MinimalSubscriber::float_topic_callback, this, _1));
+        "float_topic", 10,
+        std::bind(&MinimalSubscriber::float_topic_callback, this, _1));
   }
 
 private:
-  void float_topic_callback(const std_msgs::msg::Float32::SharedPtr msg) const
-  {
+  void float_topic_callback(const std_msgs::msg::Float32::SharedPtr msg) const {
     RCLCPP_INFO(this->get_logger(), "I heard: '%u'", interpolate(msg->data));
   }
   rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr subscription_;
 };
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalSubscriber>());
   rclcpp::shutdown();
