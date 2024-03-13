c3662a32dfb1d0e1a11ccd1ad1d7ca404c13b624
Alekskl <Aleksander.klu@hotmail.com>
2024-03-13 21:28:03 +0100

    Fixed thruster mapping

--- motion/thruster_interface/src/thruster_interface_node.cpp
+++ motion/thruster_interface/src/thruster_interface_node.cpp
@@ -20,9 +20,9 @@ void ThrusterInterfaceROS::thrust_callback(
   vortex_msgs::msg::Pwm pwm_msg;
   // TODO: Get mapping and offsets from rosparam
   // Give thrust to thruster 0: publish on pin = thruster_to_pin_map[0]
-  std::vector<int> thruster_to_pin_map = {1, 3, 2, 0};
-  std::vector<int> thruster_direction_map = {1, 1, 1, -1};
-  std::vector<int> pwm_offsets = {100, 100, 100, 100};
+  std::vector<int> thruster_to_pin_map = {3, 2, 1, 0};
+  std::vector<int> thruster_direction_map = {1,-1,-1, 1};
+  std::vector<int> pwm_offsets = {0, 0, 0, 0};
 
   // Iterates through thruster 0 to 3, where 0 is front right, iterated
   // clockwise
@@ -34,8 +34,8 @@ void ThrusterInterfaceROS::thrust_callback(
     int pwm_value_correct_direction =
         center_pwm_value + thruster_direction_map[i] * offset_from_center_value;
 
-    int pwm_clamped = std::min(std::max(pwm_value_correct_direction, 1400),
-                               1600); // min 1100, max 1900
+    int pwm_clamped = std::min(std::max(pwm_value_correct_direction, 1300),
+                               1700); // min 1100, max 1900
     pwm_msg.positive_width_us.push_back(pwm_clamped);
     pwm_msg.pins.push_back(thruster_to_pin_map[i]);
   }
