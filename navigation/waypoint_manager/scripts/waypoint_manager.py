0651272354cde652bbc7ca948b2cec27ab9d919a
Alice <alicez@stud.ntnu.no>
2024-03-17 15:25:40 +0100

    feat: removed code that is no longer going to be used

--- navigation/waypoint_manager/scripts/waypoint_manager.py
+++ navigation/waypoint_manager/scripts/waypoint_manager.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 import rclpy
-from rclpy.action import ActionClient
 from rclpy.node import Node
-
-from vortex_msgs.action import LosPathFollowing
-
 from vortex_msgs.srv import Waypoint
 from nav_msgs.msg import Path
 from geometry_msgs.msg import PoseStamped, Point
@@ -32,14 +28,10 @@ class WaypointManager(Node):
         self.waypoint_service = self.create_service(Waypoint, 'waypoint', self.waypoint_callback)
         self.waypoint_list_client = self.create_client(Waypoint, 'waypoint_list')
 
-        # Service to send waypoints
-        # self.send_waypoint_service = self.create_service(Waypoint, 'send_waypoint_service', self.send_waypoint_service_callback)
-
         # Services
         self.get_logger().info('kommet til services')
         self.add_waypoint_service = self.create_service(Waypoint, 'add_waypoint', self.add_waypoint_callback)
         self.get_logger().info('kommet til etter at man har laget add_waypoint_services')
-        self.remove_waypoint_service = self.create_service(Waypoint, 'remove_waypoint', self.remove_waypoint_callback)
 
         # nav_msgs Path to visualize LOS in Rviz
         self.path_pub = self.create_publisher(Path, 'los_path', 10)
@@ -83,62 +75,6 @@ class WaypointManager(Node):
         response.success = True
         return response
 
-    
-    def remove_waypoint_from_list(self, req):
-        """
-        Removes a waypoint from the waypoint list.
-
-        Args:
-            req (Waypoint.Request): Request containing the waypoint to be removed.
-        """
-
-        self.get_logger().info("request: " + str(req.waypoint))
-        self.get_logger().info("before remove: " + str(self.waypoint_list))
-
-        # Checks if the waypoint_list is empty
-        if not self.waypoint_list:
-            self.get_logger().info("Waypoint list is empty")
-
-            response = Waypoint.Response()
-            response.success = False
-            return response
-
-        # Checks if the waypoint exists in the list, and removes it if it exists
-        waypoint_list_len = len(self.waypoint_list)
-
-        if waypoint_list_len == 1 and ([req.waypoint[0], req.waypoint[1]] == self.waypoint_list[0]):
-                self.waypoint_list.remove(self.waypoint_list[0])
-                self.get_logger().info("remove waypoint from waypoint_list")
-        else:
-            for i in range(0, waypoint_list_len-1):
-                if [req.waypoint[0], req.waypoint[1]] == self.waypoint_list[i]:
-                    self.waypoint_list.remove(self.waypoint_list[i])
-                    self.get_logger().info("remove waypoint from waypoint_list")
-            if waypoint_list_len == len(self.waypoint_list):
-                self.get_logger().info("Waypoint not found in waypoint_list")
-
-                response = Waypoint.Response()
-                response.success = False
-                return response
-        
-        self.get_logger().info("after remove: " +str(self.waypoint_list))
-
-        # Check if path.poses is empty before popping
-        if self.path.poses:
-            self.path.poses.reverse()
-            self.path.poses.pop()
-            self.path.poses.reverse()
-
-            self.path_pub.publish(self.path)
-
-        response = Waypoint.Response()
-        response.success = True
-        
-        return response
-    
-    def remove_waypoint_callback(self, request, response):
-        return self.remove_waypoint_from_list(request)
-
     def add_waypoint_callback(self, request, response):
         self.get_logger().info(str(request))
         return self.add_waypoint_to_list(request)
@@ -147,10 +83,12 @@ class WaypointManager(Node):
         while not self.waypoint_list_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Waypoint service not available, waiting again...')
         request = Waypoint.Request()
+
         self.temp_waypoint_list = []
         for waypoint in self.waypoint_list:
             self.temp_waypoint_list.append(waypoint[0])
             self.temp_waypoint_list.append(waypoint[1])
+        
         request.waypoints = self.temp_waypoint_list
         future = self.waypoint_list_client.call_async(request)
 
@@ -160,46 +98,6 @@ class WaypointManager(Node):
             self.get_logger().info(f'Waypoints successfully submitted: {response.success}')
         except Exception as e:
             self.get_logger().error('Service call failed %r' % (e,))
-    
-    """
-    def remove_reached_waypoints(self, current_position):
-       
-        Remove waypoints from the list that have been reached by the robot.
-        
-        if not self.waypoint_list:
-            return
-        
-        # Calculate the distance between the current position and each waypoint
-        distance_threshold = 0.1  # Define a threshold for considering a waypoint reached
-        remaining_waypoints = []
-        for waypoint in self.waypoint_list:
-            distance_to_waypoint = ((waypoint[0] - current_position.x) ** 2 + (waypoint[1] - current_position.y) ** 2) ** 0.5
-            if distance_to_waypoint > distance_threshold:
-                remaining_waypoints.append(waypoint)
-        
-        # Update the waypoint list
-        self.waypoint_list = remaining_waypoints
-
-        # Publish the updated waypoints for visualization
-        self.path.poses = []
-        for waypoint in self.waypoint_list:
-            new_pose = PoseStamped()
-            new_pose.pose.position = Point(x=waypoint[0], y=waypoint[1], z=0.0)
-            self.path.poses.append(new_pose)
-        self.path_pub.publish(self.path)
-
-    def send_waypoint_service_callback(self, request, response, current_position):
-        
-        Service callback function to send the waypoint list.
-        
-        self.remove_reached_waypoints(current_position)  # Pass the current position of the robot
-        # Populate the response with the waypoint list
-        response.waypoints = []
-        for waypoint in self.waypoint_list:
-            response.waypoints.extend(waypoint)
-        
-        return response
-"""
 
 def main(args=None):
     rclpy.init(args=args)
