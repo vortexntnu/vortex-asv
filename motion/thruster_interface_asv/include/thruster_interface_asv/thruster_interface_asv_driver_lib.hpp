58d2939517fe35ff2898dfc9ede72abec26bdcf3
RoseKapps <rose.j.kapps@gmail.com>
2024-03-18 00:04:06 +0100

    added ros node

--- motion/thruster_interface_asv/include/thruster_interface_asv/thruster_interface_asv_driver_lib.hpp
+++ motion/thruster_interface_asv/include/thruster_interface_asv/thruster_interface_asv_driver_lib.hpp
@@ -1,19 +1,20 @@
 // C++ Libraries
-#include <algorithm>
-#include <fstream>
-#include <iostream>
-#include <sstream>
 #include <stdint.h>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
 
-#include <fcntl.h>         // Used for the O_RDWR define
+#include <fcntl.h>      // Used for the O_RDWR define
 #include <linux/i2c-dev.h> // Used for the I2C_SLAVE define
 #include <sys/ioctl.h>
-#include <unistd.h> // Used for the close function
+#include <unistd.h>     // Used for the close function
+
 
 // Declare functions we can use
 namespace thruster_interface_asv_driver_lib {
-void init(const std::string &pathToCSVFile, int16_t *minPWM, int16_t *maxPWM);
-int16_t *drive_thrusters(float *forces);
-} // namespace thruster_interface_asv_driver_lib
+    void init(const std::string& pathToCSVFile, int16_t* minPWM, int16_t* maxPWM);
+    int16_t* drive_thrusters(float* forces);
+}
--- motion/thruster_interface_asv/src/thruster_interface_asv_node.cpp
+++ motion/thruster_interface_asv/src/thruster_interface_asv_node.cpp
@@ -1,62 +1,91 @@
 // Custom libraies
 #include <thruster_interface_asv/thruster_interface_asv_node.hpp>
 
-int main() {
-  // Get MAX and MIN PWM values
-  int16_t minPWM[4] = {1100, 1100, 1100, 1100};
-  int16_t maxPWM[4] = {1900, 1900, 1900, 1900};
+class ThrusterInterfaceASVNode : public rclcpp::Node {
+  private:
+    // ROS2 Variables ----------
+    // Creates ROS2 subscriber
+    rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr _subscriberThrusterForces;
+    // Create ROS2 publisher
+    rclcpp::Publisher<std_msgs::msg::Int16MultiArray>::SharedPtr _publisherPWM;
+    // Create ROS2 timer/cycler
+    rclcpp::TimerBase::SharedPtr _thruster_driver_timer;
 
-  // Get filepath of .CSV file
-  std::string forcesToPWMDataFilePath =
-      ament_index_cpp::get_package_share_directory("thruster_interface_asv");
-  forcesToPWMDataFilePath += "/config/ThrustMe_P1000_force_mapping.csv";
+    // Variables that are shared inside the object ----------
+    // Mutable allows modification in const methods
+    mutable float _thrusterForces[4]; 
+    mutable int16_t* _pwm;
 
-  // Initialize Thruster driver
-  thruster_interface_asv_driver_lib::init(forcesToPWMDataFilePath, minPWM,
-                                          maxPWM);
+    // Methods ----------
+    void _thruster_forces_callback(const std_msgs::msg::Float32MultiArray::SharedPtr msg) const {
+      // Save subscribed topic values into universal array for further usage 
+      for (size_t i = 0; i < 4; i++) {
+        _thrusterForces[i] = msg->data[i];
+      }
+    }
 
-  // Test (START) --------------------------------------------------
-  float test_input[4];
-  int16_t *test_output;
+    void _thruster_driver_callback() {
+      // Drive thrusters according to the thrusterForces provided by subscriber
+      _pwm = thruster_interface_asv_driver_lib::drive_thrusters(_thrusterForces);
 
-  // Test 1 ----------
-  test_input[0] = 0.0;
-  test_input[1] = 0.0;
-  test_input[2] = 0.0;
-  test_input[3] = 0.0;
-  test_output = thruster_interface_asv_driver_lib::drive_thrusters(test_input);
-  std::cout << "Test 1: ";
-  std::cout << test_output[0] << ", ";
-  std::cout << test_output[1] << ", ";
-  std::cout << test_output[2] << ", ";
-  std::cout << test_output[3] << std::endl;
-  sleep(10);
+      // Publish PWM values for debuging purposes
+      std::shared_ptr<std_msgs::msg::Int16MultiArray> messagePWM = std::make_shared<std_msgs::msg::Int16MultiArray>();
+      messagePWM->data.resize(4);
+      for (int8_t i = 0; i < 4; i++) {
+        messagePWM->data[i] = _pwm[i];
+      }
+      _publisherPWM->publish(*messagePWM);
+    }
+  public:
+    // Builder for the object ----------
+    ThrusterInterfaceASVNode() : Node("thruster_interface_asv_node") {
+      // Thruster Driver Setup ----------
+      // Get MAX and MIN PWM values
+      int16_t minPWM[4] = {1100, 1100, 1100, 1100};
+      int16_t maxPWM[4] = {1900, 1900, 1900, 1900};
 
-  // Test 2 ----------
-  test_input[0] = 100.0;
-  test_input[1] = 100.0;
-  test_input[2] = 100.0;
-  test_input[3] = 100.0;
-  test_output = thruster_interface_asv_driver_lib::drive_thrusters(test_input);
-  std::cout << "Test 2: ";
-  std::cout << test_output[0] << ", ";
-  std::cout << test_output[1] << ", ";
-  std::cout << test_output[2] << ", ";
-  std::cout << test_output[3] << std::endl;
-  sleep(3);
+      // Get filepath of .CSV file with ROS2 file path finder
+      std::string forcesToPWMDataFilePath = ament_index_cpp::get_package_share_directory("thruster_interface_asv");
+      forcesToPWMDataFilePath += "/config/ThrustMe_P1000_force_mapping.csv";
 
-  // Test 3 ----------
-  test_input[0] = 0.0;
-  test_input[1] = 0.0;
-  test_input[2] = 0.0;
-  test_input[3] = 0.0;
-  test_output = thruster_interface_asv_driver_lib::drive_thrusters(test_input);
-  std::cout << "Test 3: ";
-  std::cout << test_output[0] << ", ";
-  std::cout << test_output[1] << ", ";
-  std::cout << test_output[2] << ", ";
-  std::cout << test_output[3] << std::endl;
+      // Initialize Thruster driver
+      thruster_interface_asv_driver_lib::init(forcesToPWMDataFilePath, minPWM, maxPWM);
 
-  delete[] test_output;
-  // Test (STOP) --------------------------------------------------
+      // ROS Setup ----------
+      // Initialize ROS2 subscribers role
+      _subscriberThrusterForces = this->create_subscription<std_msgs::msg::Float32MultiArray>(
+        "/thrust/thruster_forces", 
+        5, 
+        std::bind(&ThrusterInterfaceASVNode::_thruster_forces_callback, this, std::placeholders::_1)
+        );
+
+      // Initialize ROS2 publisher role
+      _publisherPWM = this->create_publisher<std_msgs::msg::Int16MultiArray>(
+        "/pwm", 
+        5
+        );
+
+      // Initialize a never ending cycle that continuisly publishes and drives thrusters depending on what the ThrusterForces value is set to from ThrusterForces subscriber
+      using namespace std::chrono_literals;
+      _thruster_driver_timer = this->create_wall_timer(
+        500ms, 
+        std::bind(&ThrusterInterfaceASVNode::_thruster_driver_callback, this)
+        );
+
+      // Debugging ----------
+      RCLCPP_INFO(this->get_logger(), "Initialized thruster_interface_asv_node node");
+    }
+};
+
+int main(int argc, char * argv[])
+{
+  // Start ROS2
+  rclcpp::init(argc, argv);
+
+  // Start infinite loop of ROS2 Node
+  rclcpp::spin(std::make_shared<ThrusterInterfaceASVNode>());
+
+  // Shut down ROS2
+  rclcpp::shutdown();
+  return 0;
 }
