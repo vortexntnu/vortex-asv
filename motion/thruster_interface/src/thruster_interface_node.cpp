cc516d79ba834311b9d0a89d470f3b7a904b9f22
“Vincent” <vincent.rosenpflanzer@gmail.com>
2024-03-17 16:57:10 +0100

    added publishing of temperature and status, not connected to i2c yet

--- motion/thruster_interface/src/thruster_interface_node.cpp
+++ motion/thruster_interface/src/thruster_interface_node.cpp
@@ -1,7 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float32.hpp"
+#include "std_msgs/msg/int8.hpp"
 #include <thruster_interface/thruster_interface.hpp>
-//#include <vortex_msgs/msg/pwm.hpp>
 #include <vortex_msgs/msg/thruster_forces.hpp>
 
 using std::placeholders::_1;
@@ -9,17 +9,27 @@ using std::placeholders::_1;
 constexpr double newton_to_gram_conversion_factor = 101.97162;
 
 bool started = false;
-int8_t hardware_status = 0;
+//int8_t hardware_status = 0;
 uint8_t software_killswitch = 0;
 
-class MinimalSubscriber : public rclcpp::Node {
+class ThrusterInterface : public rclcpp::Node {
 public:
-  MinimalSubscriber() : Node("thruster_interface_node") {
+  ThrusterInterface() : Node("thruster_interface_node") {
+
     subscription_ = this->create_subscription<vortex_msgs::msg::ThrusterForces>(
         "thruster_topic", 10,
-        // std::bind(&MinimalSubscriber::float_topic_callback, this, _1));
-        std::bind(&MinimalSubscriber::thrusterForcesCallback, this,
+        std::bind(&ThrusterInterface::thrusterForcesCallback, this,
                   std::placeholders::_1));
+
+    publisher_ESC1 = this->create_publisher<std_msgs::msg::Float32>("/asv/temperature/ESC1", 10);
+    publisher_ESC2 = this->create_publisher<std_msgs::msg::Float32>("/asv/temperature/ESC2", 10);
+    publisher_ESC3 = this->create_publisher<std_msgs::msg::Float32>("/asv/temperature/ESC3", 10);
+    publisher_ESC4 = this->create_publisher<std_msgs::msg::Float32>("/asv/temperature/ESC4", 10);
+    publisher_status = this->create_publisher<std_msgs::msg::Int8>("/asv/status", 10);
+
+    timer_ = this->create_wall_timer(
+      std::chrono::seconds(1),
+      std::bind(&ThrusterInterface::publish_temperature_and_status, this));
   }
 
 private:
@@ -44,39 +54,81 @@ private:
     //----------------------------------------------------------
 
     int file;
-    /**if (started == false) {
-      init(file);
-      started = true;
-    }**/
-
     init(file);
 
     // SENDING
     send_status(software_killswitch, file);
     send_pwm(pwm_values, file);
 
-    // RECEIVING
+    
+  }
 
-    std::vector<float> temperature;
+  void publish_temperature_and_status()
+    {
 
-    temperature = readFloatsFromI2C(file);
+        // RECEIVING
 
-    for (size_t i = 0; i < temperature.size(); i++) {
-      std::cout << i << "temperature value = " << temperature[i] << std::endl;
+        //int file;
+        //init(file);
+
+        //TEMPERATURE
+
+        std::vector<float> temperature = {22.04, 43.04, 34.99, 49.66};
+
+        //temperature = readFloatsFromI2C(file);
+
+        for (size_t i = 0; i < temperature.size(); i++) {
+          std::cout << i << "temperature value = " << temperature[i] << std::endl;
+        }
+
+        //hardware_status = read_hardware_statusFromI2C(file);
+        //cout << "hardware status = " << (uint16_t)(hardware_status) << endl;
+
+        
+        auto message_ESC1 = std_msgs::msg::Float32();
+        auto message_ESC2 = std_msgs::msg::Float32();
+        auto message_ESC3 = std_msgs::msg::Float32();
+        auto message_ESC4 = std_msgs::msg::Float32();
+
+        message_ESC1.data = temperature[0];
+        message_ESC2.data = temperature[1];
+        message_ESC3.data = temperature[2];
+        message_ESC4.data = temperature[3];
+
+        RCLCPP_INFO(this->get_logger(), "Publishing temperature");
+        publisher_ESC1->publish(message_ESC1);
+        publisher_ESC2->publish(message_ESC2);
+        publisher_ESC3->publish(message_ESC3);
+        publisher_ESC4->publish(message_ESC4);
+
+        //HARDWARE STATUS
+
+        int8_t hardware_status = 0;
+
+        auto message_status = std_msgs::msg::Int8();
+
+        message_status.data = hardware_status;
+
+        RCLCPP_INFO(this->get_logger(), "Publishing status");
+
+        publisher_status->publish(message_status);
+
+        //close(file);
     }
 
-    hardware_status = read_hardware_statusFromI2C(file);
-    cout << "hardware status = " << (uint16_t)(hardware_status) << endl;
+  rclcpp::Subscription<vortex_msgs::msg::ThrusterForces>::SharedPtr subscription_;
 
-    close(file);
-  }
-  rclcpp::Subscription<vortex_msgs::msg::ThrusterForces>::SharedPtr
-      subscription_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher_ESC1;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher_ESC2;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher_ESC3;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher_ESC4;
+  rclcpp::Publisher<std_msgs::msg::Int8>::SharedPtr publisher_status;
+  rclcpp::TimerBase::SharedPtr timer_;
 };
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::spin(std::make_shared<ThrusterInterface>());
   rclcpp::shutdown();
   return 0;
 }
