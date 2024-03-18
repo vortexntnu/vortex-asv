a7d216153e7fecb498653f62388ba2d364a98006
Clang Robot <robot@example.com>
2024-03-18 01:15:13 +0000

    Committing clang-format changes

--- motion/thruster_interface_asv/include/thruster_interface_asv/thruster_interface_asv_driver_lib.hpp
+++ motion/thruster_interface_asv/include/thruster_interface_asv/thruster_interface_asv_driver_lib.hpp
@@ -1,20 +1,21 @@
 // C++ Libraries
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 #include <stdint.h>
 #include <string>
 #include <vector>
-#include <fstream>
-#include <sstream>
-#include <iostream>
-#include <algorithm>
 
-#include <fcntl.h>      // Used for the O_RDWR define
+#include <fcntl.h>         // Used for the O_RDWR define
 #include <linux/i2c-dev.h> // Used for the I2C_SLAVE define
 #include <sys/ioctl.h>
-#include <unistd.h>     // Used for the close function
-
+#include <unistd.h> // Used for the close function
 
 // Declare functions we can use
 namespace thruster_interface_asv_driver_lib {
-    void init(const std::string& pathToCSVFile, int8_t* thrusterMapping, int8_t* thrusterDirection, int16_t* offsetPWM, int16_t* minPWM, int16_t* maxPWM);
-    int16_t* drive_thrusters(float* thrusterForces);
-}
+void init(const std::string &pathToCSVFile, int8_t *thrusterMapping,
+          int8_t *thrusterDirection, int16_t *offsetPWM, int16_t *minPWM,
+          int16_t *maxPWM);
+int16_t *drive_thrusters(float *thrusterForces);
+} // namespace thruster_interface_asv_driver_lib
--- motion/thruster_interface_asv/include/thruster_interface_asv/thruster_interface_asv_node.hpp
+++ motion/thruster_interface_asv/include/thruster_interface_asv/thruster_interface_asv_node.hpp
@@ -2,12 +2,11 @@
 #include <vector>
 
 // ROS2 libraries
+#include <ament_index_cpp/get_package_share_directory.hpp>
+#include <chrono> // For the ROS2 Timer clock
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/float32_multi_array.hpp>
 #include <std_msgs/msg/int16_multi_array.hpp>
-#include <ament_index_cpp/get_package_share_directory.hpp>
-#include <chrono> // For the ROS2 Timer clock
 
 // Custom libraries
 #include <thruster_interface_asv/thruster_interface_asv_driver_lib.hpp>
-
