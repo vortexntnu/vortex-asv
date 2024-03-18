525c0d133784d6163d0ee72dcc6efef44e54a8d9
Alice <alicez@stud.ntnu.no>
2024-03-18 18:40:04 +0100

    feat: added doc-strings

--- navigation/waypoint_manager/scripts/waypoint_manager.py
+++ navigation/waypoint_manager/scripts/waypoint_manager.py
@@ -9,18 +9,16 @@ from scripts.Waypoint2D import Waypoint2D
 
 class WaypointManager(Node):
     """
-    Nodes created:
-        WaypointManager
+    Nodes created: WaypointManager
     Subscribes to: d_star_lite
     Publishes to: hybridpath_guidance
     """
     
     def __init__(self):
         """
-        Initializes WaypointManager class.
-
-        Initializes node, sets up action client, services, and publishers.
+        Initializes the WaypointManager node, sets up services, clients, and publishers.
         """
+
         super().__init__("WaypointManager")
 
         self.waypoint_list = []
@@ -33,21 +31,35 @@ class WaypointManager(Node):
         self.path.header.frame_id = 'world'
 
     def waypoint_callback(self, request, response):
-        self.waypoint_list.clear()
+        """
+        Clears the current waypoint list and adds new waypoints from the request.
 
+        Args:
+            request: The request message containing waypoints.
+            response: The response message indicating the success of the operation.
+
+        Returns:
+            The response message with the success status.
+        """
+
+        self.waypoint_list.clear()
         return self.add_waypoint_to_list(request)
     
    
     def add_waypoint_to_list(self, req):
         """
-        Adds waypoints to the waypoint list.
+        Adds waypoints to the waypoint list from the request.
+
+        This method processes a request containing a list of waypoints, converting them into `Waypoint2D` objects,
+        and appending them to the waypoint list. It also updates the `path` with new poses.
 
         Args:
             req (Waypoint.Request): Request containing the list of waypoints to be added.
 
         Returns:
-            Waypoint.Response: True if waypoints are added successfully.
+            Waypoint.Response: A response object indicating success.
         """
+
         new_waypoints = list(zip(req.waypoint[::2], req.waypoint[1::2]))
         self.waypoint_list.extend([Waypoint2D(north=n, east=e) for n, e in new_waypoints])
 
@@ -63,10 +75,30 @@ class WaypointManager(Node):
         return response
 
     def add_waypoint_callback(self, request, response):
-        self.get_logger().info(str(request))
+        """
+        Callback function for the add_waypoint service.
+
+        Forwards the request to `add_waypoint_to_list` method.
+
+        Args:
+            request: The request object containing new waypoints.
+            response: The response object to be filled by `add_waypoint_to_list`.
+
+        Returns:
+            The response from `add_waypoint_to_list`.
+        """
+
         return self.add_waypoint_to_list(request)
     
     def sending_waypoints(self):
+        """
+        Sends the current list of waypoints to the waypoint list service.
+
+        Waits for the waypoint list service to become available. Once available,
+        it sends the current list of waypoints and waits for a response. Logs the
+        outcome of the service call.
+        """
+
         while not self.waypoint_list_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Waypoint service not available, waiting again...')
         request = Waypoint.Request()
