27bec425d1d297140e316fbda47e84ef6a13f4c6
Andeshog <157955984+Andeshog@users.noreply.github.com>
2024-03-19 21:09:17 +0100

    Added more descriptive tests

--- guidance/d_star_lite/tests/test_d_star_lite.py
+++ guidance/d_star_lite/tests/test_d_star_lite.py
@@ -1,7 +1,7 @@
 import unittest
 import pytest
-from d_star_lite.d_star_lite import DStarLite, DSLNode
-from d_star_lite.d_star_lite import combine_nodes, compare_coordinates, distance
+from d_star_lite.d_star_lite import DStarLite
+from d_star_lite.d_star_lite_node import DSLNode
 import numpy as np
 
 class TestDStarLite(unittest.TestCase):
@@ -15,68 +15,86 @@ class TestDStarLite(unittest.TestCase):
         # Create example obstacle coordinates
         self.ox = [1, 2, 3, 4, 5]
         self.oy = [0, 0, 0, 0, 0]
+        
+        # Create start and goal
+        self.start = DSLNode(0, 0)
+        self.goal = DSLNode(5, 5)
 
         # Create example dsl object
-        self.dsl = DStarLite(self.ox, self.oy)
+        self.dsl = DStarLite(self.ox, self.oy, self.start, self.goal)
 
     def tearDown(self):
         pass
 
-    def test_combine_DSLNodes(self):
-        # Test the combine_DSLNodes function
-        result = combine_nodes(self.DSLNode1, self.DSLNode2)
-        self.assertEqual(result.x, 1)
-        self.assertEqual(result.y, 1)
-        self.assertEqual(result.cost, 5.4)
+    # Test node addition
+    def test_combine_DSLNodes_with_positive_values(self):
+        self.assertEqual(self.DSLNode1 + self.DSLNode2, DSLNode(1, 1, 5.4))
 
-        result = combine_nodes(self.DSLNode2, self.DSLNode5)
-        self.assertEqual(result.x, 0)
-        self.assertEqual(result.y, 0)
-        self.assertEqual(result.cost, -2.4)
+    def test_combine_DSLNodes_with_negative_values(self):
+        self.assertEqual(self.DSLNode5 + self.DSLNode5, DSLNode(-2, -2, -11.6))
 
-    def test_compare_coordinates(self):
-        # Test the compare_coordinates function
-        result = compare_coordinates(self.DSLNode1, self.DSLNode2)
-        self.assertEqual(result, False)
+    def test_combine_DSLNodes_with_positive_and_negative_values(self):
+        self.assertEqual(self.DSLNode1 + self.DSLNode5, DSLNode(-1, -1, -3.8))
 
-        result = compare_coordinates(self.DSLNode1, self.DSLNode1)
-        self.assertEqual(result, True)
+    # Test node comparison
+    def test_compare_coordinates_is_false_for_two_different_nodes(self):
+        
+        self.assertEqual(self.DSLNode1 == self.DSLNode2, False)
+    
+    def test_compare_coordinates_is_true_for_two_same_nodes(self):
+        self.assertEqual(self.DSLNode1 == self.DSLNode1, True)
 
-    def test_distance(self):
-        # Test the distance function
-        result = distance(self.DSLNode1, self.DSLNode2)
+    # Test the distance function
+    def test_distance_between_two_nodes_yields_correct_euclidean_distance(self):
+        
+        result = DSLNode.distance_between_nodes(self.DSLNode1, self.DSLNode2)
         self.assertEqual(result, np.sqrt(2))
 
-        result = distance(self.DSLNode2, self.DSLNode5)
+        result = DSLNode.distance_between_nodes(self.DSLNode2, self.DSLNode5)
         self.assertEqual(result, np.sqrt(8))
 
-    def test_is_obstacle(self):
-        # Test the is_obstacle function
+    # Test the is_obstacle function
+    def test_is_obstacle_is_true_when_node_is_obstacle(self):
+        
         self.assertEqual(self.dsl.is_obstacle(DSLNode(1, 0)), True)
-        self.assertEqual(self.dsl.is_obstacle(DSLNode(2, 0)), True)
-        self.assertEqual(self.dsl.is_obstacle(DSLNode(5, 0)), True)
+
+    def test_is_obstacle_is_true_when_node_is_in_safe_distance_from_obstacle(self):
+        self.assertEqual(self.dsl.is_obstacle(DSLNode(2, 2)), True)
+
+    def test_is_obstacle_is_false_when_node_is_not_obstacle(self):
         self.assertEqual(self.dsl.is_obstacle(DSLNode(10, 0)), False)
 
-    def test_movement_cost(self):
-        # Test the movement_cost function
+    # Test the movement_cost function
+    def test_movement_cost_when_moving_straight(self):
+        
         self.assertEqual(self.dsl.movement_cost(DSLNode(10, 0), DSLNode(11, 0)), 1.0)
-        self.assertEqual(self.dsl.movement_cost(DSLNode(10, 0), DSLNode(10, 1)), 1.0)
+
+    def test_movement_cost_when_moving_diagonally(self):
         self.assertEqual(self.dsl.movement_cost(DSLNode(10, 10), DSLNode(11, 11)), np.sqrt(2))
+
+    def test_movement_cost_when_moving_to_obstacle(self):
         self.assertEqual(self.dsl.movement_cost(DSLNode(1, 0), DSLNode(2, 0)), np.inf)
 
+    # Test the heuristic_distance function (distance to target node)
     def test_heuristic_distance(self):
-        # Test the heuristic_distance function
-        self.dsl.goal = DSLNode(5, 5)
         self.assertEqual(self.dsl.heuristic_distance(DSLNode(0, 0)), np.sqrt(50))
         self.assertEqual(self.dsl.heuristic_distance(DSLNode(5, 5)), 0.0)
         self.assertEqual(self.dsl.heuristic_distance(DSLNode(10, 10)), np.sqrt(50))
+    # Test the get_direction function
+    def test_get_direction_when_moving_in_positive_x_direction(self):
+        self.assertEqual(DSLNode.get_direction(DSLNode(0, 0), DSLNode(1, 0)), (1, 0))
 
-    def test_get_direction(self):
-        # Test the get_direction function
-        self.assertEqual(self.dsl.get_direction(DSLNode(0, 0), DSLNode(1, 0)), (1, 0))
-        self.assertEqual(self.dsl.get_direction(DSLNode(0, 0), DSLNode(2, 2)), (1, 1))
-        self.assertEqual(self.dsl.get_direction(DSLNode(0, 0), DSLNode(-1, 0)), (-1, 0))
-        self.assertEqual(self.dsl.get_direction(DSLNode(0, 0), DSLNode(0, -1)), (0, -1))
+    def test_get_direction_when_moving_positive_diagonal(self):
+        self.assertEqual(DSLNode.get_direction(DSLNode(0, 0), DSLNode(2, 2)), (1, 1))
+
+    def test_get_direction_when_moving_in_negative_x_direction(self):
+        self.assertEqual(DSLNode.get_direction(DSLNode(0, 0), DSLNode(-1, 0)), (-1, 0))
+
+    def test_get_direction_when_moving_in_negative_y_direction(self):
+        self.assertEqual(DSLNode.get_direction(DSLNode(0, 0), DSLNode(0, -1)), (0, -1))
+
+    def test_get_direction_when_moving_in_negative_diagonal(self):
+        self.assertEqual(DSLNode.get_direction(DSLNode(0, 0), DSLNode(-2, -2)), (-1, -1))
 
 if __name__ == '__main__':
     unittest.main()
