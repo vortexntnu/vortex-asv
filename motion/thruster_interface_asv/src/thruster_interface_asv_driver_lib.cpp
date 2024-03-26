81f974ae95b2c438efca40ea9f2ab47cb42e3aea
vortexuser <vortex.git@vortexntnu.no>
2024-03-26 19:10:20 +0100

    fixed mapping

--- motion/thruster_interface_asv/src/thruster_interface_asv_driver_lib.cpp
+++ motion/thruster_interface_asv/src/thruster_interface_asv_driver_lib.cpp
@@ -192,6 +192,16 @@ void init(const std::string &pathToCSVFile, int8_t *thrusterMapping,
 
 // The main core functionality of interacting and controling the thrusters
 int16_t *drive_thrusters(float *thrusterForces) {
+
+
+  // Change direction of the thruster (Forward(+1)/Backwards(-1)) according to
+  // the direction parameter
+  float thrusterForcesChangedDirection[4] = {0.0, 0.0, 0.0, 0.0};
+  for (int8_t i = 0; i < 4; i++) {
+    thrusterForcesChangedDirection[i] =
+        thrusterForces[i] * _thrusterDirection[i];
+  }
+  
   // Remap Thrusters
   // From: [pin1:thruster1, pin2:thruster2, pin3:thruster3, pin4:thruster4]
   // To:   [pin1:<specifiedThruster>, pin2:<specifiedThruster>,
@@ -200,19 +210,11 @@ int16_t *drive_thrusters(float *thrusterForces) {
   for (int8_t pinNr = 0; pinNr < 4; pinNr++) {
     int8_t remapedThrusterForcesIndex = _thrusterMapping[pinNr];
     thrusterForcesChangedMapping[pinNr] =
-        thrusterForces[remapedThrusterForcesIndex];
-  }
-
-  // Change direction of the thruster (Forward(+1)/Backwards(-1)) according to
-  // the direction parameter
-  float thrusterForcesChangedDirection[4] = {0.0, 0.0, 0.0, 0.0};
-  for (int8_t i = 0; i < 4; i++) {
-    thrusterForcesChangedDirection[i] =
-        thrusterForcesChangedMapping[i] * _thrusterDirection[i];
+        thrusterForcesChangedDirection[remapedThrusterForcesIndex];
   }
 
   // Interpolate forces to raw PWM values
-  int16_t *pwm = _interpolate_force_to_pwm(thrusterForcesChangedDirection);
+  int16_t *pwm = _interpolate_force_to_pwm(thrusterForcesChangedMapping);
 
   // Offset PWM
   for (int8_t i = 0; i < 4; i++) {
