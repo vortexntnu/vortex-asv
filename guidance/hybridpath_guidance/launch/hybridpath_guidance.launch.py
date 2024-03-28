ae0f8e70395e401714b91457b998ab5d73db98dc
Martin Huynh <martiniuss.wan@gmail.com>
2024-03-28 21:47:34 +0100

    Merge branch 'hybridpath_to_CMakeLists'

--- guidance/hybridpath_guidance/launch/hybridpath_guidance.launch.py
+++ guidance/hybridpath_guidance/launch/hybridpath_guidance.launch.py
@@ -6,7 +6,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     hybridpath_guidance_node = Node(
             package='hybridpath_guidance',
-            executable='hybridpath_guidance_node',
+            executable='hybridpath_guidance_node.py',
             name='hybridpath_guidance_node',
             parameters=[os.path.join(get_package_share_directory('hybridpath_guidance'),'config','hybridpath_guidance_config.yaml')],
             output='screen',
--- motion/hybridpath_controller/launch/hybridpath_controller.launch.py
+++ motion/hybridpath_controller/launch/hybridpath_controller.launch.py
@@ -6,7 +6,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     hybridpath_controller_node = Node(
             package='hybridpath_controller',
-            executable='hybridpath_controller_node',
+            executable='hybridpath_controller_node.py',
             name='hybridpath_controller_node',
             parameters=[os.path.join(get_package_share_directory('hybridpath_controller'),'config','hybridpath_controller_config.yaml')],
             output='screen',
