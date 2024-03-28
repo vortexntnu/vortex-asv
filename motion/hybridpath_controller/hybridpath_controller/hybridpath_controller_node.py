4dee06203d901cf19d46fad635edee8702447835
Andeshog <157955984+Andeshog@users.noreply.github.com>
2024-03-28 16:20:37 +0100

    Moved K1 and K2 to yaml

--- motion/hybridpath_controller/hybridpath_controller/hybridpath_controller_node.py
+++ motion/hybridpath_controller/hybridpath_controller/hybridpath_controller_node.py
@@ -1,4 +1,5 @@
 import rclpy
+import numpy as np
 from rclpy.node import Node
 from hybridpath_controller.adaptive_backstep import AdaptiveBackstep
 from geometry_msgs.msg import Wrench
@@ -8,12 +9,34 @@ from vortex_msgs.msg import HybridpathReference
 class HybridPathControllerNode(Node):
     def __init__(self):
         super().__init__("hybridpath_controller_node")
+
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ("hybridpath_controller.K1_diag", [10, 10, 10]),
+                ("hybridpath_controller.K2_diag", [60, 60, 60]),
+                ("hybridpath_controller.M_diag", [50, 50, 50]),
+                ("hybridpath_controller.D_diag", [10, 10, 5])
+            ])
         
         self.state_subscriber_ = self.state_subscriber_ = self.create_subscription(Odometry, "/sensor/seapath/odometry/ned", self.state_callback, 1)
         self.hpref_subscriber_ = self.create_subscription(HybridpathReference, "guidance/hybridpath/reference", self.reference_callback, 1)
         self.wrench_publisher_ = self.create_publisher(Wrench, "thrust/wrench_input", 1)
 
-        self.AB_controller_ = AdaptiveBackstep()
+        # Get parameters
+        K1_diag = self.get_parameter("hybridpath_controller.K1_diag").get_parameter_value().double_array_value
+        K2_diag = self.get_parameter("hybridpath_controller.K2_diag").get_parameter_value().double_array_value
+        M_diag = self.get_parameter("hybridpath_controller.M_diag").get_parameter_value().double_array_value
+        D_diag = self.get_parameter("hybridpath_controller.D_diag").get_parameter_value().double_array_value
+
+        # Transform parameters to diagonal matrices
+        K1 = np.diag(K1_diag)
+        K2 = np.diag(K2_diag)
+        M = np.diag(M_diag)
+        D = np.diag(D_diag)
+
+        # Create controller object
+        self.AB_controller_ = AdaptiveBackstep(K1, K2, M, D)
 
         controller_period = 0.1
         self.controller_timer_ = self.create_timer(controller_period, self.controller_callback)
