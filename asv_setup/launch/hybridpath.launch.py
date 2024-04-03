b2bb3e82cb87e5e3f8f8687dea82557b3ed8a17f
Martin Huynh <martiniuss.wan@gmail.com>
2024-04-03 18:54:45 +0200

    Fixed asv_setup hybridpath nitpick

--- asv_setup/launch/hybridpath.launch.py
+++ asv_setup/launch/hybridpath.launch.py
@@ -8,13 +8,13 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     hybridpath_controller_launch = IncludeLaunchDescription(
     PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('hybridpath_controller'), 'launch/hybridpath_controller.launch.py')
+            os.path.join(get_package_share_directory('hybridpath_controller'), 'launch', 'hybridpath_controller.launch.py')
         )
     )
 
     hybridpath_guidance_launch = IncludeLaunchDescription(
     PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('hybridpath_guidance'), 'launch/hybridpath_guidance.launch.py')
+            os.path.join(get_package_share_directory('hybridpath_guidance'), 'launch', 'hybridpath_guidance.launch.py')
         )
     )
 
