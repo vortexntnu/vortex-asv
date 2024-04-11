f3267b58ac364450e35d59b7731e0daafc239703
Andeshog <157955984+Andeshog@users.noreply.github.com>
2024-04-11 08:51:23 +0200

    Fix node

--- guidance/hybridpath_guidance/hybridpath_guidance/hybridpath_guidance_node.py
+++ guidance/hybridpath_guidance/hybridpath_guidance/hybridpath_guidance_node.py
@@ -40,9 +40,8 @@ class Guidance(Node):
 
     def waypoint_callback(self, request, response):
         self.get_logger().info('Received waypoints, generating path...')
-        waypoints = request.waypoint
-        self.waypoints_array = np.array([[waypoint.x, waypoint.y] for waypoint in waypoints])
-        generator = HybridPathGenerator(self.waypoints_array, self.path_generator_order, self.lambda_val)
+        self.waypoints = request.waypoint
+        generator = HybridPathGenerator(self.waypoints, self.path_generator_order, self.lambda_val)
         self.path = generator.Path
         self.waypoints_received = True
         self.waiting_message_printed = False  # Reset this flag to handle multiple waypoint sets
