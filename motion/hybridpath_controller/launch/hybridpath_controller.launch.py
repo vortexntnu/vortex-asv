e7fbf97bbacfb47a34ae15908b0af8a06903aa78
Martin Huynh <martiniuss.wan@gmail.com>
2024-04-14 12:21:44 +0200

    Fixed import params from freya.yaml

--- motion/hybridpath_controller/launch/hybridpath_controller.launch.py
+++ motion/hybridpath_controller/launch/hybridpath_controller.launch.py
@@ -8,7 +8,10 @@ def generate_launch_description():
             package='hybridpath_controller',
             executable='hybridpath_controller_node.py',
             name='hybridpath_controller_node',
-            parameters=[os.path.join(get_package_share_directory('hybridpath_controller'),'config','hybridpath_controller_config.yaml')],
+            parameters=[
+                os.path.join(get_package_share_directory('hybridpath_controller'), 'config', 'hybridpath_controller_config.yaml'),
+                os.path.join(get_package_share_directory('asv_setup'), 'config', 'robots', 'freya.yaml')
+            ],
             output='screen',
         )
     return LaunchDescription([
