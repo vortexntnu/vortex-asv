2cff782a5c0a65eb7868fb0fba70b8827e28362b
Martin Huynh <martiniuss.wan@gmail.com>
2024-04-15 20:02:38 +0200

    Added matplotlib plotting, progress bar, giraffe

--- simulation/asv_simulator/launch/asv_simulator.launch.py
+++ simulation/asv_simulator/launch/asv_simulator.launch.py
@@ -2,16 +2,9 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, LogInfo
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    declare_plotting_method_arg = DeclareLaunchArgument(
-        'plotting_method',
-        default_value='foxglove',
-        description='Plotting method to use: matplotlib or foxglove')
-
     asv_simulator_node = Node(
             package='asv_simulator',
             executable='asv_simulator_node.py',
@@ -19,10 +12,9 @@ def generate_launch_description():
             output='screen',
             parameters=[
                 os.path.join(get_package_share_directory('asv_setup'), 'config', 'robots', 'freya.yaml'),
-                {'plotting_method': LaunchConfiguration('plotting_method')}]
+                os.path.join(get_package_share_directory('asv_simulator'), 'config', 'asv_sim_config.yaml')]
         )
 
     return LaunchDescription([
-        declare_plotting_method_arg,
         asv_simulator_node
     ])
