6883fd3eb26f361c38f6f81ef5e61ae52558e424
jorgenfj <jorgfje@stud.ntnu.no>
2024-04-20 12:57:57 +0200

    fixed lidar and zed tf

--- asv_setup/launch/tf.launch.py
+++ asv_setup/launch/tf.launch.py
@@ -15,15 +15,15 @@ def generate_launch_description():
                    '--z'              , '-0.392425',
                    '--roll'           , str(math.pi),
                    '--pitch'          , '0',
-                   '--yaw'            , '0',
+                   '--yaw'            , str(math.pi),
                    '--frame-id'       , 'base_link',
-                   '--child-frame-id' , 'os_lidar'],
+                   '--child-frame-id' , 'os_sensor'],
     )
 
-    # base_link to zed2i_camera_center tf.
-    tf_base_link_to_zed2_camera_center = Node(
+    # base_link to zed_camera_link tf.
+    tf_base_link_to_zed_camera_link = Node(
         package='tf2_ros',
-        name='base_link_to_zed2_camera_center',
+        name='base_link_to_zed_camera_link',
         executable='static_transform_publisher',
         arguments=['--x'              , '0.3005',
                    '--y'              , '0',
@@ -32,7 +32,7 @@ def generate_launch_description():
                    '--pitch'          , '0',
                    '--yaw'            , '0',
                    '--frame-id'       , 'base_link',
-                   '--child-frame-id' , 'zed2i_camera_center'],
+                   '--child-frame-id' , 'zed_camera_link'],
     )
 
     # base_link (NED) to seapath_frame (NED) tf.
@@ -52,6 +52,6 @@ def generate_launch_description():
 
     return LaunchDescription([
         tf_base_link_to_lidar,
-        tf_base_link_to_zed2_camera_center,
+        tf_base_link_to_zed_camera_link,
         tf_base_link_to_seapath,
     ])
