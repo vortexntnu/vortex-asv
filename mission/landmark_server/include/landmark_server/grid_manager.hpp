883f88da88e51ae4d714ee06112152f998eee2cd
jorgenfj <jorgfje@stud.ntnu.no>
2024-08-17 17:39:09 +0200

    moved map-odom-tf to map_manager and added optional grid usage in landmark server

--- mission/landmark_server/include/landmark_server/grid_manager.hpp
+++ mission/landmark_server/include/landmark_server/grid_manager.hpp
@@ -16,20 +16,28 @@ public:
 
   ~GridManager() = default;
 
+  /**
+   * @brief Update the grid with a polygon and a value to update corresponding cells with.
+   */
   void update_grid(int8_t *grid,
-                   const Eigen::Array<float, 2, Eigen::Dynamic> &polygon,
+                   const Eigen::Array<float, 2, Eigen::Dynamic> &vertices,
                    int value);
 
-  void handle_polygon(int8_t *grid,
-                      const Eigen::Array<float, 2, Eigen::Dynamic> &vertices,
-                      int value);
-
+  /**
+   * @brief Draw a line on the grid and update the value of the cells it passes through.
+   */
   void draw_line(int x0, int y0, int x1, int y1, int8_t *grid, int value);
 
+  /**
+   * @brief Fill a polygon on the grid and update the value of the cells it covers.
+   */
   void fill_polygon(int8_t *grid,
                     const Eigen::Array<float, 2, Eigen::Dynamic> &polygon,
                     int value);
 
+  /**
+   * @brief Check if a point is inside a polygon.
+   */
   bool point_in_polygon(int x, int y,
                         const Eigen::Array<float, 2, Eigen::Dynamic> &polygon);
 
