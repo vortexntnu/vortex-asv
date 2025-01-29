382967c54612aeda742a2f5d9416886ee2148794
Andreas Kluge Svendsrud <kluge7@users.noreply.github.com>
2025-01-29 12:08:36 +0100

    feat(docker): added script for building and running docker images

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-source /vortex_ws/devel/setup.bash
-roslaunch asv_setup naglfar.launch type:=simulator
+# This script builds and runs a Docker image
+
+# Define environment variables for the Docker build process and execute the build script
+IMAGE="asv-image:latest" \
+BASE_IMAGE="rwthika/ros2:humble" \
+TARGET="dev" \
+PLATFORM="linux/arm64,linux/amd64" \
+./docker/docker-ros/scripts/build.sh
+
+# Run the built Docker image with appropriate flags
+docker run -it --rm \
+    --privileged \
+    --network host \
+    --ipc=host \
+    -v "$(pwd):/docker-ros/ws/src/target:rshared" \
+    "asv-image:latest" /bin/bash
