b6edfb87dacaa2938ae2b78c2df41b01b1c2750f
Alberto Morselli <albertomorselli00@gmail.com>
2025-02-25 22:06:35 +0100

    now compiles

--- motion/thruster_interface_asv/src/thruster_interface_asv_driver.cpp
+++ motion/thruster_interface_asv/src/thruster_interface_asv_driver.cpp
@@ -1,141 +1,134 @@
-#include "thruster_interface_asv/thruster_interface_asv_ros.hpp"
+#include "thruster_interface_asv/thruster_interface_asv_driver.hpp"
 
-ThrusterInterfaceASVNode::ThrusterInterfaceASVNode()
-    : Node("thruster_interface_asv_node") {
-    // extract all .yaml parameters
-    this->extract_all_parameters();
+ThrusterInterfaceASVDriver::ThrusterInterfaceASVDriver(
+    short i2c_bus,
+    int pico_i2c_address,
+    const std::vector<ThrusterParameters>& thruster_parameters,
+    const std::vector<std::vector<double>>& poly_coeffs)
+    : i2c_bus_(i2c_bus),
+      pico_i2c_address_(pico_i2c_address),
+      thruster_parameters_(thruster_parameters),
+      poly_coeffs_(poly_coeffs) {
+    std::string i2c_filename = "/dev/i2c-" + std::to_string(i2c_bus_);
+    bus_fd_ =
+        open(i2c_filename.c_str(),
+             O_RDWR);  // Open the i2c bus for reading and writing (0_RDWR)
+    if (bus_fd_ < 0) {
+        std::runtime_error("ERROR: Failed to open I2C bus " +
+                           std::to_string(i2c_bus_) + " : " +
+                           std::string(strerror(errno)));
+    }
 
-    // Set up subscriber and publisher
-    rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
-    auto qos_sensor_data = rclcpp::QoS(
-        rclcpp::QoSInitialization(qos_profile.history, 1), qos_profile);
-
-    thruster_forces_subscriber_ =
-        this->create_subscription<vortex_msgs::msg::ThrusterForces>(
-            subscriber_topic_name_, qos_sensor_data,
-            std::bind(&ThrusterInterfaceASVNode::thruster_forces_callback, this,
-                      std::placeholders::_1));
-
-    thruster_pwm_publisher_ =
-        this->create_publisher<std_msgs::msg::Int16MultiArray>(
-            publisher_topic_name_, qos_sensor_data);
-
-    // call constructor for thruster_driver_
-    thruster_driver_ = std::make_unique<ThrusterInterfaceASVDriver>(
-        i2c_bus_, i2c_address_, thruster_parameters_, poly_coeffs_);
-
-    // Declare thruster_forces_array_ in case no topic comes at the
-    // very beginning
-    thruster_forces_array_ = std::vector<double>(4, 0.00);
-
-    this->initialize_parameter_handler();
-
-    RCLCPP_INFO(this->get_logger(),
-                "\"thruster_interface_asv_node\" correctly initialized");
+    idle_pwm_value_ =
+        (calc_poly(0, poly_coeffs_[LEFT]) + calc_poly(0, poly_coeffs_[RIGHT])) /
+        2;
 }
 
-void ThrusterInterfaceASVNode::thruster_forces_callback(
-    const vortex_msgs::msg::ThrusterForces::SharedPtr msg) {
-    thruster_forces_array_ = msg->thrust;
-    this->pwm_callback();
+ThrusterInterfaceASVDriver::~ThrusterInterfaceASVDriver() {
+    if (bus_fd_ >= 0) {
+        send_data_to_escs(std::vector<uint16_t>(thruster_parameters_.size(),
+                                                idle_pwm_value_));
+        close(bus_fd_);
+    }
 }
 
-void ThrusterInterfaceASVNode::pwm_callback() {
-    // drive thrusters...
+std::vector<uint16_t> ThrusterInterfaceASVDriver::interpolate_forces_to_pwm(
+    const std::vector<double>& thruster_forces_array) {
+    // Convert Newtons to Kg (since the thruster datasheet is in Kg)
+    std::vector<double> forces_in_kg(thruster_forces_array.size());
+    std::transform(thruster_forces_array.begin(), thruster_forces_array.end(),
+                   forces_in_kg.begin(), to_kg);
+
+    std::vector<uint16_t> interpolated_pwm;
+    for (const double force : forces_in_kg) {
+        interpolated_pwm.push_back(force_to_pwm(force, poly_coeffs_));
+    }
+    return interpolated_pwm;
+}
+
+std::uint16_t ThrusterInterfaceASVDriver::force_to_pwm(
+    double force,
+    const std::vector<std::vector<double>>& coeffs) {
+    if (force < 0) {
+        return calc_poly(force, coeffs[LEFT]);
+    } else if (force > 0) {
+        return calc_poly(force, coeffs[RIGHT]);
+    } else {
+        return idle_pwm_value_;  // 1500
+    }
+}
+
+std::uint16_t ThrusterInterfaceASVDriver::calc_poly(
+    double force,
+    const std::vector<double>& coeffs) {
+    return static_cast<std::uint16_t>(coeffs[0] * std::pow(force, 3) +
+                                      coeffs[1] * std::pow(force, 2) +
+                                      coeffs[2] * force + coeffs[3]);
+}
+
+void ThrusterInterfaceASVDriver::send_data_to_escs(
+    const std::vector<uint16_t>& thruster_pwm_array) {
+    constexpr std::size_t i2c_data_size =
+        1 + 8 * 2;  // 8 thrusters * (1xMSB + 1xLSB)
+    std::vector<std::uint8_t> i2c_data_array;
+    i2c_data_array.reserve(i2c_data_size);
+
+    i2c_data_array.push_back(0x00);  // Start byte
+    std::for_each(thruster_pwm_array.begin(), thruster_pwm_array.end(),
+                  [&](std::uint16_t pwm) {
+                      std::array<std::uint8_t, 2> bytes = pwm_to_i2c_data(pwm);
+                      std::copy(bytes.begin(), bytes.end(),
+                                std::back_inserter(i2c_data_array));
+                  });
+
+    // Set the I2C slave address
+    if (ioctl(bus_fd_, I2C_SLAVE, pico_i2c_address_) < 0) {
+        throw std::runtime_error("Failed to open I2C bus " +
+                                 std::to_string(i2c_bus_) + " : " +
+                                 std::string(strerror(errno)));
+        return;
+    }
+
+    // Write data to the I2C device
+    if (write(bus_fd_, i2c_data_array.data(), i2c_data_size) != i2c_data_size) {
+        throw std::runtime_error("ERROR: Failed to write to I2C device : " +
+                                 std::string(strerror(errno)));
+    }
+}
+
+std::vector<uint16_t> ThrusterInterfaceASVDriver::drive_thrusters(
+    const std::vector<double>& thruster_forces_array) {
+    // Apply thruster mapping and direction
+    std::vector<double> mapped_forces(thruster_forces_array.size());
+    for (size_t i = 0; i < thruster_parameters_.size(); ++i) {
+        mapped_forces[i] =
+            thruster_forces_array[thruster_parameters_[i].mapping] *
+            thruster_parameters_[i].direction;
+    }
+
+    // Convert forces to PWM
     std::vector<uint16_t> thruster_pwm_array =
-        thruster_driver_->drive_thrusters(this->thruster_forces_array_);
+        interpolate_forces_to_pwm(mapped_forces);
 
-    //..and publish PWM values for debugging purposes
-    if (debug_flag_) {
-        std_msgs::msg::Int16MultiArray pwm_message;
-        pwm_message.data = std::vector<int16_t>(thruster_pwm_array.begin(),
-                                                thruster_pwm_array.end());
-        thruster_pwm_publisher_->publish(pwm_message);
-    }
-}
-
-void ThrusterInterfaceASVNode::initialize_parameter_handler() {
-    param_handler_ = std::make_shared<rclcpp::ParameterEventHandler>(this);
-
-    // Register the parameter event callback directly in the lambda expression
-    debug_flag_parameter_cb = param_handler_->add_parameter_callback(
-        "debug.flag", std::bind(&ThrusterInterfaceASVNode::update_debug_flag,
-                                this, std::placeholders::_1));
-}
-
-void ThrusterInterfaceASVNode::update_debug_flag(const rclcpp::Parameter& p) {
-    debug_flag_ = p.get_value<bool>();
-    RCLCPP_INFO(this->get_logger(),
-                "Received parameter event: debug.flag updated to: %s",
-                debug_flag_ ? "true" : "false");
-}
-
-void ThrusterInterfaceASVNode::extract_all_parameters() {
-    // thruster parameters from orca.yaml
-    this->declare_parameter<std::vector<int>>(
-        "propulsion.thrusters.thruster_to_pin_mapping");
-    this->declare_parameter<std::vector<int>>(
-        "propulsion.thrusters.thruster_direction");
-    this->declare_parameter<std::vector<int>>(
-        "propulsion.thrusters.thruster_PWM_min");
-    this->declare_parameter<std::vector<int>>(
-        "propulsion.thrusters.thruster_PWM_max");
-
-    // approx poly coeffs for 16V from thruster_interface_asv.yaml
-    this->declare_parameter<std::vector<double>>("coeffs.LEFT");
-    this->declare_parameter<std::vector<double>>("coeffs.RIGHT");
-
-    // i2c parameters
-    this->declare_parameter<int>("i2c.bus");
-    this->declare_parameter<int>("i2c.address");
-
-    // topics
-    this->declare_parameter<std::string>("topics.thruster_forces");
-    this->declare_parameter<std::string>("topics.pwm_output");
-
-    this->declare_parameter<bool>("debug.flag");
-
-    //-----------------------------------------------------------------------
-    // get them
-    auto thruster_mapping =
-        this->get_parameter("propulsion.thrusters.thruster_to_pin_mapping")
-            .as_integer_array();
-    auto thruster_direction =
-        this->get_parameter("propulsion.thrusters.thruster_direction")
-            .as_integer_array();
-    auto thruster_PWM_min =
-        this->get_parameter("propulsion.thrusters.thruster_PWM_min")
-            .as_integer_array();
-    auto thruster_PWM_max =
-        this->get_parameter("propulsion.thrusters.thruster_PWM_max")
-            .as_integer_array();
-
-    std::vector<double> left_coeffs =
-        this->get_parameter("coeffs.LEFT").as_double_array();
-    std::vector<double> right_coeffs =
-        this->get_parameter("coeffs.RIGHT").as_double_array();
-
-    this->i2c_bus_ = this->get_parameter("i2c.bus").as_int();
-    this->i2c_address_ = this->get_parameter("i2c.address").as_int();
-
-    this->subscriber_topic_name_ =
-        this->get_parameter("topics.thruster_forces").as_string();
-    this->publisher_topic_name_ =
-        this->get_parameter("topics.pwm_output").as_string();
-
-    this->debug_flag_ = this->get_parameter("debug.flag").as_bool();
-
-    // create <ThrusterParameters> and <PolyCoeffs> vectors
-    ThrusterParameters temp;
-    for (size_t i = 0; i < thruster_mapping.size(); ++i) {
-        temp.mapping = static_cast<uint8_t>(thruster_mapping[i]);
-        temp.direction = static_cast<int8_t>(thruster_direction[i]);
-        temp.pwm_min = static_cast<uint16_t>(thruster_PWM_min[i]);
-        temp.pwm_max = static_cast<uint16_t>(thruster_PWM_max[i]);
-
-        this->thruster_parameters_.push_back(temp);
+    // Apply thruster offset and limit PWM if needed
+    for (size_t i = 0; i < thruster_pwm_array.size(); ++i) {
+        // Clamp the PWM signal
+        if (thruster_pwm_array[i] < thruster_parameters_[i].pwm_min) {
+            thruster_pwm_array[i] = thruster_parameters_[i].pwm_min;
+        } else if (thruster_pwm_array[i] > thruster_parameters_[i].pwm_max) {
+            thruster_pwm_array[i] = thruster_parameters_[i].pwm_max;
+        }
     }
 
-    this->poly_coeffs_.push_back(left_coeffs);
-    this->poly_coeffs_.push_back(right_coeffs);
+    try {
+        send_data_to_escs(thruster_pwm_array);
+    } catch (const std::exception& e) {
+        std::cerr << "ERROR: Failed to send PWM values - " << e.what()
+                  << std::endl;
+    } catch (...) {
+        std::cerr << "ERROR: Failed to send PWM values - Unknown exception"
+                  << std::endl;
+    }
+
+    return thruster_pwm_array;
 }
